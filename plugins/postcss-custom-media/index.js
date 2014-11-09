32767c6e1d2221ca990a090e76ad15cb0489a275
Maxime Thirouin <m@moox.io>
2014-11-09 07:29:25 +0100

    Adjust gnuMessage if no source available

--- plugins/postcss-custom-media/index.js
+++ plugins/postcss-custom-media/index.js
@@ -65,5 +65,5 @@ function customMedia(options) {
  * @param {Object} source
  */
 function gnuMessage(message, source) {
-  return (source ? (source.file ? source.file : "<css input>") + ":" + source.start.line + ":" + source.start.column : "") + " " + message
+  return (source ? (source.file ? source.file : "<css input>") + ":" + source.start.line + ":" + source.start.column + " " : "") + message
 }
