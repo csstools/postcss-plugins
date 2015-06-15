8ef604f6550fa80c9b405a7e425ed51249097a68
Jonathan Neal <jonathantneal@hotmail.com>
2015-06-15 09:12:02 -0400

    Update version
    Update index.js: use postcss-selector-parser
    Update package.json: use postcss-selector-parser and bump version
    Update tests.js: postcss-selector-parser preserves selector separators

--- plugins/postcss-pseudo-class-any-link/index.js
+++ plugins/postcss-pseudo-class-any-link/index.js
@@ -1,15 +1,35 @@
 var postcss = require('postcss');
+var postcssSelectorParser = require('postcss-selector-parser');
 
 module.exports = postcss.plugin('postcss-pseudo-class-any-link', function (opts) {
-	var match = new RegExp(':' + (opts && opts.prefix ? '-' + opts.prefix + '-' : '') + 'any-link\\b', 'g');
+	var pseudoValue = ':' + (opts && opts.prefix ? '-' + opts.prefix + '-' : '') + 'any-link';
+	var pseudoFallbackValues = [':link', ':visited'];
+
+	function eachRule(rule) {
+		rule.selector = postcssSelectorParser(function (selectors) {
+			selectors.each(function (selector, index) {
+				var originalIndex = index;
+
+				pseudoFallbackValues.forEach(function (pseudoFallbackValue) {
+					var clone = selector.clone();
+
+					clone.eachPseudo(function (pseudo) {
+						if (pseudo.value === pseudoValue) {
+							pseudo.value = pseudoFallbackValue;
+
+							selectors.nodes.splice(++index, 0, clone);
+						}
+					});
+				});
+
+				if (originalIndex !== index) {
+					selector.removeSelf();
+				}
+			});
+		}).process(rule.selector).result;
+	}
 
 	return function (css) {
-		css.eachRule(function (rule) {
-			if (match.test(rule.selector)) {
-				rule.selector = ['link', 'visited'].map(function (replacement) {
-					return rule.selector.replace(match, ':' + replacement);
-				}).join(', ');
-			}
-		});
+		css.eachRule(eachRule);
 	};
 });
--- plugins/postcss-pseudo-class-any-link/test/test.js
+++ plugins/postcss-pseudo-class-any-link/test/test.js
@@ -15,7 +15,7 @@ var test = function (input, output, opts, done) {
 
 describe('postcss-pseudo-class-any-link', function () {
 	it(':any-link transforms to :link and :visited', function (done) {
-		test('ul a:any-link > span { background: yellow; }', 'ul a:link > span, ul a:visited > span { background: yellow; }', {}, done);
+		test('ul a:any-link > span { background: yellow; }', 'ul a:link > span,ul a:visited > span { background: yellow; }', {}, done);
 	});
 
 	it(':any-link remains :any-link { prefix: "foo" }', function (done) {
@@ -25,7 +25,7 @@ describe('postcss-pseudo-class-any-link', function () {
 	});
 
 	it(':-foo-any-link transforms to :link and :visited { prefix: "foo" }', function (done) {
-		test('ul a:-foo-any-link > span { background: yellow; }', 'ul a:link > span, ul a:visited > span { background: yellow; }', {
+		test('ul a:-foo-any-link > span { background: yellow; }', 'ul a:link > span,ul a:visited > span { background: yellow; }', {
 			prefix: 'foo'
 		}, done);
 	});
