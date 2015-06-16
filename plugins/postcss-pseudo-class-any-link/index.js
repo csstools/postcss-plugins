7bb162418a07e09590e12e073778b40506dda795
Jonathan Neal <jonathantneal@hotmail.com>
2015-06-15 23:40:12 -0400

    Update plugin
    Added: Support for complex uses
    Added: Code documentation
    Changed: Coding conventions

--- plugins/postcss-pseudo-class-any-link/index.js
+++ plugins/postcss-pseudo-class-any-link/index.js
@@ -2,34 +2,50 @@ var postcss = require('postcss');
 var postcssSelectorParser = require('postcss-selector-parser');
 
 module.exports = postcss.plugin('postcss-pseudo-class-any-link', function (opts) {
-	var pseudoValue = ':' + (opts && opts.prefix ? '-' + opts.prefix + '-' : '') + 'any-link';
-	var pseudoFallbackValues = [':link', ':visited'];
-
-	function eachRule(rule) {
-		rule.selector = postcssSelectorParser(function (selectors) {
-			selectors.each(function (selector, index) {
-				var originalIndex = index;
-
-				pseudoFallbackValues.forEach(function (pseudoFallbackValue) {
-					var clone = selector.clone();
-
-					clone.eachPseudo(function (pseudo) {
-						if (pseudo.value === pseudoValue) {
-							pseudo.value = pseudoFallbackValue;
-
-							selectors.nodes.splice(++index, 0, clone);
-						}
-					});
-				});
-
-				if (originalIndex !== index) {
-					selector.removeSelf();
-				}
-			});
-		}).process(rule.selector).result;
-	}
+	// cache the any-link value
+	var valueAnyLink = ':' + (opts && opts.prefix ? '-' + opts.prefix + '-' : '') + 'any-link';
 
 	return function (css) {
-		css.eachRule(eachRule);
+		// for each rule
+		css.eachRule(function (rule) {
+			// update the selector
+			rule.selector = postcssSelectorParser(function (selectors) {
+				// cache variables
+				var node;
+				var nodeIndex;
+				var selector;
+				var selectorLink;
+				var selectorVisited;
+
+				// cache the selector index
+				var selectorIndex = -1;
+
+				// for each selector
+				while ((selector = selectors.nodes[++selectorIndex])) {
+					// reset the node index
+					nodeIndex = -1;
+
+					// for each node
+					while ((node = selector.nodes[++nodeIndex])) {
+						// if the node value matches the any-link value
+						if (node.value === valueAnyLink) {
+							// clone the selector
+							selectorLink = selector.clone();
+							selectorVisited = selector.clone();
+
+							// update the matching clone values
+							selectorLink.nodes[nodeIndex].value = ':link';
+							selectorVisited.nodes[nodeIndex].value = ':visited';
+
+							// replace the selector with the clones and roll back the selector index
+							selectors.nodes.splice(selectorIndex--, 1, selectorLink, selectorVisited);
+
+							// stop updating the selector
+							break;
+						}
+					}
+				}
+			}).process(rule.selector).result;
+		});
 	};
 });
