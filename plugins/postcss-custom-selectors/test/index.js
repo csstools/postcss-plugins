e5500dab06f4371cfabfc78d1534f4e6cd6499c2
一丝 <percyley@qq.com>
2015-07-01 10:25:54 +0800

    Format

--- plugins/postcss-custom-selectors/test/index.js
+++ plugins/postcss-custom-selectors/test/index.js
@@ -1,26 +1,29 @@
-var fs = require("fs")
-
-var test = require("tape")
-
+var fs      = require("fs")
+var test    = require("tape")
 var postcss = require("postcss")
-var plugin = require("..")
+var plugin  = require("..")
 
-function filename(name) { return "test/" + name + ".css" }
-function read(name) { return fs.readFileSync(name, "utf8") }
+function filename(name) {
+  return "test/" + name + ".css"
+}
+
+function read(name) {
+  return fs.readFileSync(name, "utf8")
+}
 
 function compareFixtures(t, name, msg, opts, postcssOpts) {
   postcssOpts = postcssOpts || {}
-  //input
+    //input
   postcssOpts.from = filename("fixtures/" + name + "/input")
   opts = opts || {}
   var result = postcss()
-  .use(plugin(opts))
-  .process(read(postcssOpts.from), postcssOpts)
+    .use(plugin(opts))
+    .process(read(postcssOpts.from), postcssOpts)
 
   var actual = result.css
-  //output
+    //output
   var output = read(filename("fixtures/" + name + "/output"))
-  //actual
+    //actual
   fs.writeFile(filename("fixtures/" + name + "/actual"), actual)
   t.equal(actual.trim(), output.trim(), msg)
 
@@ -28,25 +31,25 @@ function compareFixtures(t, name, msg, opts, postcssOpts) {
 }
 
 test("@custom-selector", function(t) {
-  compareFixtures(t, "heading", "should transform heading")
-  compareFixtures(t, "pseudo", "should transform pseudo")
-  compareFixtures(t, "multiline", "should transform multiline")
-  compareFixtures(t, "some-hyphen", "should transform some hyphen")
-  compareFixtures(t, "matches", "should transform matches selector")
-  var similarMatchesResult = compareFixtures(t, "similar-matches", "should transform matches selector")
+  compareFixtures(t, "heading", "Should transform heading")
+  compareFixtures(t, "pseudo", "Should transform pseudo")
+  compareFixtures(t, "multiline", "Should transform multiline")
+  compareFixtures(t, "some-hyphen", "Should transform some hyphen")
+  compareFixtures(t, "matches", "Should transform matches selector")
+  var similarMatchesResult = compareFixtures(t, "similar-matches", "Should transform matches selector")
   t.ok(
     similarMatchesResult.messages && similarMatchesResult.messages.length === 1,
-    "should add a message when a custom selectors is undefined"
+    "Should add a message when a custom selectors is undefined"
   )
 
-  compareFixtures(t, "comment", "should transform comment")
-  compareFixtures(t, "line-break", "should transform line break", {
+  compareFixtures(t, "comment", "Should transform comment")
+  compareFixtures(t, "line-break", "Should transform line break", {
     lineBreak: false
   })
 
-  compareFixtures(t, "extension", "should transform local extensions", {
+  compareFixtures(t, "extension", "Should transform local extensions", {
     extensions: {
-      ':--any' : 'section, article, aside, nav',
+      ':--any': 'section, article, aside, nav',
       ':--foo': 'input[type="text"] > section, #nav .bar'
     }
   })
