adc9cb7ceb89d7b3b3b5d8ba4530039933c911f5
Jonathan Neal <jonathantneal@hotmail.com>
2015-09-15 09:22:39 -0400

    Update tests
    Follow Tab Atkins new syntax
    Update to PostCSS 5 and PostCSS 5 best practices

--- plugins/postcss-nesting/test/test.js
+++ plugins/postcss-nesting/test/test.js
@@ -1,24 +1,36 @@
 var postcss = require('postcss');
+var nested  = require('../');
 var expect  = require('chai').expect;
 
-var plugin = require('../');
-
-var test = function (input, output, opts, done) {
-	postcss([ plugin(opts) ]).process(input).then(function (result) {
-		expect(result.css).to.eql(output);
-		expect(result.warnings()).to.be.empty;
-		done();
-	}).catch(function (error) {
-		done(error);
-	});
+var check = function (input, output, opts) {
+	var processor = postcss([ nested(opts) ]);
+	expect( processor.process(input).css ).to.equal(output);
 };
 
-describe('postcss-nesting', function () {
-	it('basic usage', function (done) {
-		test(
-			'a,b{color:red;{c,d{color:white;}& &{color:blue;}&:hover{color:white;}}}',
-			'a,b{color:red}a c,b c,a d,b d{color:white;}a a,a b,b a,b b{color:blue;}a:hover,b:hover{color:white;}',
-			{},
-		done);
+describe('postcss-nested', function () {
+	it('ignores sass nesting', function () {
+		check('z { color: red } a, b { color: white; & c, & d { color: blue } }',
+			  'z { color: red } a, b { color: white; & c, & d { color: blue } }');
+	});
+
+	it('unwraps css nesting', function () {
+		check('z { color: red } a, b { color: white; @nest & c, & d { color: blue } }',
+			  'z { color: red } a, b { color: white } a c, a d, b c, b d { color: blue }');
+	});
+
+	it('unwraps css deep nesting', function () {
+		check('z { color: red } a, b { color: white; @nest & c, & d { color: blue; @nest & e, & f { color: black } } }',
+			  'z { color: red } a, b { color: white } a c, a d, b c, b d { color: blue } a c e, a c f, a d e, a d f, b c e, b c f, b d e, b d f { color: black }');
+	});
+
+	it('unwraps mixed nesting', function () {
+		check('a { color: red; @media { color: white; @nest b { color: blue } } }',
+			  'a { color: red } @media { a { color: white } a b { color: blue } }');
+	});
+
+	it('unwraps specified at-rules', function () {
+		check('a { color: red; @unknown test { color: white } } b { color: white; @phone { color: blue } @media { color: black; @nest c { color: yellow } } }',
+			  'a { color: red; @unknown test { color: white } } b { color: white } @phone { b { color: blue } } @media { b { color: black } b c { color: yellow } }',
+			  { bubble: ['phone'] });
 	});
 });
