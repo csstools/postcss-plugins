2114394326ddd8cdbf71bc38cf7b047f72e31a2f
Jonathan Neal <jonathantneal@hotmail.com>
2016-12-14 10:14:10 -0800

    3.0.1

--- plugins/postcss-pseudo-class-any-link/CHANGELOG.md
+++ plugins/postcss-pseudo-class-any-link/CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changes to :any-link
 
+### 3.0.1 (December 8, 2016)
+
+- Updated: Use destructing assignment on plugin options
+- Updated: Use template literals
+
 ### 3.0.0 (December 5, 2016)
 
 - Updated: boilerplate conventions (Node v6.9.1 LTS)
--- plugins/postcss-pseudo-class-any-link/index.js
+++ plugins/postcss-pseudo-class-any-link/index.js
@@ -5,64 +5,69 @@ const parser = require('postcss-selector-parser');
 // plugin
 module.exports = postcss.plugin('postcss-pseudo-class-any-link', ({
 	prefix = ''
-}) => {
+} = {}) => {
 	// dashed prefix
-	const dashedPrefix = prefix ? '-' + prefix + '-' : '';
+	const dashedPrefix = prefix ? `-${ prefix }-` : '';
 
 	// any-link value
 	const anyLinkValue = `:${ dashedPrefix }any-link`;
 
-	// selector pattern
+	// selector matcher
 	const selectorMatch = new RegExp(`${ dashedPrefix }any-link`);
 
 	return (css) => {
-		// for each rule
+		// walk each matching rule
 		css.walkRules(selectorMatch, (rule) => {
 			const rawSelector = rule.raws.selector && rule.raws.selector.raw || rule.selector;
 
 			// workaround for https://github.com/postcss/postcss-selector-parser/issues/28#issuecomment-171910556
-			if (rawSelector[rawSelector.length - 1] === ':') {
-				return;
-			}
+			if (rawSelector[rawSelector.length - 1] !== ':') {
+				// update the selector
+				rule.selector = parser((selectors) => {
+					// cache variables
+					let node;
+					let nodeIndex;
+					let selector;
+					let selectorLink;
+					let selectorVisited;
 
-			// update the selector
-			rule.selector = parser((selectors) => {
-				// cache variables
-				let node;
-				let nodeIndex;
-				let selector;
-				let selectorLink;
-				let selectorVisited;
+					// cache the selector index
+					let selectorIndex = -1;
 
-				// cache the selector index
-				let selectorIndex = -1;
+					// for each selector
+					while (selector = selectors.nodes[++selectorIndex]) {
+						// reset the node index
+						nodeIndex = -1;
 
-				// for each selector
-				while (selector = selectors.nodes[++selectorIndex]) {
-					// reset the node index
-					nodeIndex = -1;
+						// for each node
+						while (node = selector.nodes[++nodeIndex]) {
+							// if the node value matches the any-link value
+							if (node.value === anyLinkValue) {
+								// clone the selector
+								selectorLink = selector.clone();
+								selectorVisited = selector.clone();
 
-					// for each node
-					while (node = selector.nodes[++nodeIndex]) {
-						// if the node value matches the any-link value
-						if (node.value === anyLinkValue) {
-							// clone the selector
-							selectorLink = selector.clone();
-							selectorVisited = selector.clone();
+								// update the matching clone values
+								selectorLink.nodes[nodeIndex].value = ':link';
+								selectorVisited.nodes[nodeIndex].value = ':visited';
 
-							// update the matching clone values
-							selectorLink.nodes[nodeIndex].value = ':link';
-							selectorVisited.nodes[nodeIndex].value = ':visited';
+								// replace the selector with the clones and roll back the selector index
+								selectors.nodes.splice(selectorIndex--, 1, selectorLink, selectorVisited);
 
-							// replace the selector with the clones and roll back the selector index
-							selectors.nodes.splice(selectorIndex--, 1, selectorLink, selectorVisited);
-
-							// stop updating the selector
-							break;
+								// stop updating the selector
+								break;
+							}
 						}
 					}
-				}
-			}).process(rawSelector).result;
+				}).process(rawSelector).result;
+			}
 		});
 	};
 });
+
+// override plugin#process
+module.exports.process = function (cssString, pluginOptions, processOptions) {
+	return postcss([
+		1 in arguments ? module.exports(pluginOptions) : module.exports()
+	]).process(cssString, processOptions);
+};
