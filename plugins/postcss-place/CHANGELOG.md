255fdffad8d2c378af53a5d813b15d1a60fd68e5
Jonathan Neal <jonathantneal@hotmail.com>
2016-12-14 10:14:07 -0800

    1.0.2

--- plugins/postcss-place/CHANGELOG.md
+++ plugins/postcss-place/CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changes to Place
 
+### 1.0.2 (December 8, 2016)
+
+- Updated: Use destructing assignment on plugin options
+- Updated: Use template literals
+
 ### 1.0.1 (December 6, 2016)
 
 - Updated: boilerplate conventions (`postcss-tape`)
--- plugins/postcss-place/index.js
+++ plugins/postcss-place/index.js
@@ -4,38 +4,51 @@ const parser  = require('postcss-value-parser');
 
 // plugin
 module.exports = postcss.plugin('postcss-place', ({
-	prefix
-} = {}) => (root) => {
-	// property
-	const prop = new RegExp(prefix ? '^-' + prefix + '-(content|items|self)' : '^place-(content|items|self)');
+	prefix = ''
+} = {}) => {
+	// dashed prefix
+	const dashedPrefix = prefix ? `-${ prefix }-` : '';
 
-	root.walkDecls(prop, (decl) => {
-		// alignment
-		const alignment = decl.prop.match(prop)[1];
+	// property matcher
+	const propertyMatch = new RegExp(`^${ dashedPrefix }place-(content|items|self)`);
 
-		// value
-		const value = parser(decl.value);
+	return (root) => {
+		// walk each matching declaration
+		root.walkDecls(propertyMatch, (decl) => {
+			// alignment
+			const alignment = decl.prop.match(propertyMatch)[1];
 
-		// divider position
-		const index = value.nodes.map(
-			(node) => node.type
-		).indexOf('space');
+			// value
+			const value = parser(decl.value);
 
-		// new justify-[alignment] and align-[alignment] declarations
-		const alignValue   = index === -1 ? decl.value : parser.stringify(value.nodes.slice(0, index));
-		const justifyValue = index === -1 ? decl.value : parser.stringify(value.nodes.slice(index + 1));
+			// divider position
+			const index = value.nodes.map(
+				(node) => node.type
+			).indexOf('space');
 
-		decl.cloneBefore({
-			prop: 'align-' + alignment,
-			value: alignValue
+			// new justify-[alignment] and align-[alignment] declarations
+			const alignValue   = index === -1 ? decl.value : parser.stringify(value.nodes.slice(0, index));
+			const justifyValue = index === -1 ? decl.value : parser.stringify(value.nodes.slice(index + 1));
+
+			decl.cloneBefore({
+				prop: `align-${ alignment }`,
+				value: alignValue
+			});
+
+			decl.cloneBefore({
+				prop: `justify-${ alignment }`,
+				value: justifyValue
+			});
+
+			// remove place-[alignment]
+			decl.remove();
 		});
-
-		decl.cloneBefore({
-			prop: 'justify-' + alignment,
-			value: justifyValue
-		});
-
-		// remove place-[alignment]
-		decl.remove();
-	});
+	};
 });
+
+// override plugin#process
+module.exports.process = function (cssString, pluginOptions, processOptions) {
+	return postcss([
+		0 in arguments ? module.exports(pluginOptions) : module.exports()
+	]).process(cssString, processOptions);
+};
