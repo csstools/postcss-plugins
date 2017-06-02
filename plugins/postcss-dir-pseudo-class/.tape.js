a0d74c4512a6c6695eadfe0eadf19667c5f157b4
Jonathan Neal <jonathantneal@hotmail.com>
2017-06-02 12:48:10 -0400

    Update Tests
    Test for “dir” and “browsers” options

--- plugins/postcss-dir-pseudo-class/.tape.js
+++ plugins/postcss-dir-pseudo-class/.tape.js
@@ -2,6 +2,29 @@ module.exports = {
 	'postcss-dir-pseudo-class': {
 		'basic': {
 			message: 'supports basic usage'
+		},
+		'basic:dir': {
+			message: 'supports { dir: "ltr" } usage',
+			source: 'basic.css',
+			options: {
+				dir: 'ltr'
+			}
+		},
+		'basic:browsers': {
+			message: 'supports { browsers: "last 2 versions" } usage',
+			source: 'basic.css',
+			expect: 'basic.expect.css',
+			result: 'basic.result.css',
+			options: {
+				browsers: 'last 2 versions'
+			}
+		},
+		'basic:supported': {
+			message: 'ignores supported { ff >= 49 } usage',
+			source: 'basic.css',
+			options: {
+				browsers: 'ff >= 49'
+			}
 		}
 	}
 };
