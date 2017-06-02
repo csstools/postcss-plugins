9ba611204bc49dbc20ae6e6f698d0a3de55bccc1
Jonathan Neal <jonathantneal@hotmail.com>
2017-06-02 12:46:41 -0400

    Update Plugin: Organize and document source

--- plugins/postcss-dir-pseudo-class/index.js
+++ plugins/postcss-dir-pseudo-class/index.js
@@ -1,22 +1,27 @@
 // tooling
-const postcss = require('postcss');
+const postcss        = require('postcss');
 const selectorParser = require('postcss-selector-parser');
 
 // plugin
-module.exports = postcss.plugin('postcss-dir-pseudo-class', () => (root) => {
+module.exports = postcss.plugin('postcss-dir-pseudo-class', (opts) => (root) => {
+	// walk rules using the :dir pseudo-class
 	root.walkRules(/:dir\([^\)]*\)/, (rule) => {
+		// update the rule selector
 		rule.selector = selectorParser((selectors) => {
+			// for each (comma separated) selector
 			selectors.nodes.forEach(
 				(selector) => {
+					// walk all selector nodes that are :dir pseudo-classes
 					selector.walk((node) => {
-						// ...
 						if ('pseudo' === node.type && ':dir' === node.value) {
+							// previous and next selector nodes
 							const prev = node.prev();
-							const prevIsSpaceCombinator = prev && prev.type && 'combinator' === prev.type && ' ' === prev.value;
-
 							const next = node.next();
+
+							const prevIsSpaceCombinator = prev && prev.type && 'combinator' === prev.type && ' ' === prev.value;
 							const nextIsSpaceCombinator = next && next.type && 'combinator' === next.type && ' ' === next.value;
 
+							// preserve the selector tree
 							if (prevIsSpaceCombinator && (nextIsSpaceCombinator || !next)) {
 								node.replaceWith(
 									selectorParser.universal()
@@ -25,6 +30,7 @@ module.exports = postcss.plugin('postcss-dir-pseudo-class', () => (root) => {
 								node.remove();
 							}
 
+							// conditionally prepend a combinator before inserting the [dir] attribute
 							const first = selector.nodes[0];
 							const firstIsSpaceCombinator = first && 'combinator' === first.type && ' ' === first.value;
 
@@ -36,11 +42,15 @@ module.exports = postcss.plugin('postcss-dir-pseudo-class', () => (root) => {
 								);
 							}
 
+							// value of the :dir pseudo-class
+							const value = node.nodes.toString();
+
+							// prepend the dir attribute
 							selector.prepend(
 								selectorParser.attribute({
 									attribute: 'dir',
 									operator: '=',
-									value: `"${ node.nodes.toString() }"`
+									value: `"${ value }"`
 								})
 							);
 						}
