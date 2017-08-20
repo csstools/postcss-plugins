77b8376e54f32887da44295f533d500fc9fe2eed
Jonathan Neal <jonathantneal@hotmail.com>
2017-08-19 20:52:52 -0400

    Add "dir" tests

--- plugins/postcss-logical/.tape.js
+++ plugins/postcss-logical/.tape.js
@@ -3,20 +3,56 @@ module.exports = {
 		'border': {
 			message: 'supports logical "border" property values'
 		},
+		'border:ltr': {
+			message: 'supports logical "border" property values with { dir: "ltr" }',
+			options: {
+				dir: 'ltr'
+			}
+		},
 		'clear': {
 			message: 'supports logical "clear" property values'
+		},
+		'clear:ltr': {
+			message: 'supports logical "clear" property values with { dir: "ltr" }',
+			options: {
+				dir: 'ltr'
+			}
 		},
 		'float': {
 			message: 'supports logical "float" property values'
 		},
+		'float:ltr': {
+			message: 'supports logical "float" property values with { dir: "ltr" }',
+			options: {
+				dir: 'ltr'
+			}
+		},
 		'inset': {
 			message: 'supports logical "inset" properties'
+		},
+		'inset:ltr': {
+			message: 'supports logical "inset" properties with { dir: "ltr" }',
+			options: {
+				dir: 'ltr'
+			}
 		},
 		'margin': {
 			message: 'supports logical "margin" properties'
 		},
+		'margin:ltr': {
+			message: 'supports logical "margin" properties with { dir: "ltr" }',
+			options: {
+				dir: 'ltr'
+			}
+		},
 		'padding': {
 			message: 'supports logical "padding" properties'
+		},
+		'padding:ltr': {
+			message: 'supports logical "padding" properties with { dir: "ltr" }',
+			options: {
+				dir: 'ltr'
+			}
 		},
 		'resize': {
 			message: 'supports logical "resize" properties'
@@ -26,6 +62,12 @@ module.exports = {
 		},
 		'text-align': {
 			message: 'supports logical "text-align" properties'
+		},
+		'text-align:ltr': {
+			message: 'supports logical "text-align" properties with { dir: "ltr" }',
+			options: {
+				dir: 'ltr'
+			}
 		}
 	}
 };
