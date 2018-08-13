9522c12ecb9b8eafd328ca2f95b196c2e29cd4f6
Pascal Duez <pascalduez@users.noreply.github.com>
2018-08-13 14:34:52 +0200

    Use PostCSS 7
    * Upgrade PostCSS to version 7
    * Add package-lock file
    * Fix broken unit tests

--- plugins/postcss-color-hex-alpha/test/index.js
+++ plugins/postcss-color-hex-alpha/test/index.js
@@ -23,7 +23,7 @@ function compareFixtures(t, name, msg, opts, postcssOpts) {
   opts = opts || {}
   var actual = transform(read(postcssOpts.from), postcssOpts)
   var expected = read(filename("fixtures/" + name + ".expected"))
-  fs.writeFile(filename("fixtures/" + name + ".actual"), actual)
+  fs.writeFileSync(filename("fixtures/" + name + ".actual"), actual)
   t.equal(actual, expected, msg)
 }
 
