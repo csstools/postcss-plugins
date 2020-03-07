896db28b5849ccc64fcba186f73ebd94eb059d8f
Yvain Liechti <ryuran@gmail.com>
2020-03-07 18:44:41 +0100

    Allow combined selectors (postcss/postcss-custom-properties#199)

--- plugins/postcss-custom-properties/src/lib/get-custom-properties-from-root.js
+++ plugins/postcss-custom-properties/src/lib/get-custom-properties-from-root.js
@@ -48,8 +48,8 @@ const rootSelectorRegExp = /^:root$/i;
 const customPropertyRegExp = /^--[A-z][\w-]*$/;
 
 // whether the node is an html or :root rule
-const isHtmlRule = node => node.type === 'rule' && htmlSelectorRegExp.test(node.selector) && Object(node.nodes).length;
-const isRootRule = node => node.type === 'rule' && rootSelectorRegExp.test(node.selector) && Object(node.nodes).length;
+const isHtmlRule = node => node.type === 'rule' && node.selector.split(',').some(item => htmlSelectorRegExp.test(item)) && Object(node.nodes).length;
+const isRootRule = node => node.type === 'rule' && node.selector.split(',').some(item => rootSelectorRegExp.test(item)) && Object(node.nodes).length;
 
 // whether the node is an custom property
 const isCustomDecl = node => node.type === 'decl' && customPropertyRegExp.test(node.prop);
