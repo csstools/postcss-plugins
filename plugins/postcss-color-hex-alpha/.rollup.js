bc50f85bd169a10bcf1ea2cccc4efed64ba5980d
Dmitry Semigradsky <semigradskyd@gmail.com>
2021-01-12 18:54:37 +0300

    Update dependencies

--- plugins/postcss-color-hex-alpha/.rollup.js
+++ plugins/postcss-color-hex-alpha/.rollup.js
@@ -1,9 +1,20 @@
-import pkg from './package.json'
+import babel from '@rollup/plugin-babel';
 
 export default {
-	...pkg.rollup,
-	plugins: pkg.rollup.plugins.map(plugin => require(plugin)()),
-	onwarn(warning, warn) {
-		if (warning.code !== 'UNRESOLVED_IMPORT') warn(warning)
-	}
-}
+	input: 'src/index.js',
+	output: [
+		{ file: 'dist/index.cjs.js', format: 'cjs', sourcemap: true, exports: 'default' },
+		{ file: 'dist/index.es.mjs', format: 'es', sourcemap: true, exports: 'default' }
+	],
+	plugins: [
+		babel({
+			babelHelpers: 'bundled',
+			plugins: [
+				'@babel/plugin-syntax-dynamic-import'
+			],
+			presets: [
+				['@babel/env', { modules: false, targets: { node: 10 } }]
+			]
+		})
+	]
+};
