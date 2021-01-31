828a5d94fa38602db644d6c9be73e9fb6e872b86
Dmitry Semigradsky <semigradskyd@gmail.com>
2021-01-31 15:54:29 +0300

    Support PostCSS 8

--- plugins/css-prefers-color-scheme/.rollup.js
+++ plugins/css-prefers-color-scheme/.rollup.js
@@ -1,4 +1,4 @@
-import babel from 'rollup-plugin-babel';
+import babel from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 
 const isBrowser = String(process.env.NODE_ENV).includes('browser');
@@ -6,7 +6,7 @@ const isBrowserMin = String(process.env.NODE_ENV).includes('browser:min');
 const isPostCSS = String(process.env.NODE_ENV).includes('postcss');
 
 // support IE9+ browsers, otherwise node 6+
-const targets = isBrowser ? 'ie >= 9' : { node: 8 };
+const targets = isBrowser ? 'ie >= 9' : { node: 10 };
 
 // read from src/browser.js for browsers/node, src/postcss.js for postcss
 const input = isPostCSS ? 'src/postcss.js' : 'src/browser.js';
@@ -26,6 +26,7 @@ const output = isPostCSS
 // use babel, and also terser to minify browser.min.js
 const plugins = [
 	babel({
+    babelHelpers: 'bundled',
 		presets: [
 			['@babel/env', { modules: false, targets }]
 		]
--- plugins/css-prefers-color-scheme/src/postcss.js
+++ plugins/css-prefers-color-scheme/src/postcss.js
@@ -1,25 +1,30 @@
-import postcss from 'postcss';
-
 const mediaRegExp = /^media$/i;
 const prefersInterfaceRegExp = /\(\s*prefers-color-scheme\s*:\s*(dark|light|no-preference)\s*\)/i;
 const colorIndexByStyle = { dark: 48, light: 70, 'no-preference': 22 };
 const prefersInterfaceReplacer = ($0, style) => `(color-index: ${colorIndexByStyle[style.toLowerCase()]})`;
 
-export default postcss.plugin('postcss-prefers-color-scheme', opts => {
+const creator = opts => {
 	const preserve = 'preserve' in Object(opts) ? opts.preserve : true;
 
-	return root => {
-		root.walkAtRules(mediaRegExp, atRule => {
-			const { params } = atRule;
-			const altParams = params.replace(prefersInterfaceRegExp, prefersInterfaceReplacer);
+	return {
+		postcssPlugin: 'postcss-prefers-color-scheme',
+		Once: root => {
+			root.walkAtRules(mediaRegExp, atRule => {
+				const { params } = atRule;
+				const altParams = params.replace(prefersInterfaceRegExp, prefersInterfaceReplacer);
 
-			if (params !== altParams) {
-				if (preserve) {
-					atRule.cloneBefore({ params: altParams });
-				} else {
-					atRule.params = altParams;
+				if (params !== altParams) {
+					if (preserve) {
+						atRule.cloneBefore({ params: altParams });
+					} else {
+						atRule.params = altParams;
+					}
 				}
-			}
-		});
-	}
-});
+			});
+		},
+	};
+};
+
+creator.postcss = true;
+
+export default creator;
