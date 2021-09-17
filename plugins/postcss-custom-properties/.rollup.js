263946178cdb520729860f68b801b2e800b57686
Jonathan Neal <jonathantneal@hotmail.com>
2021-09-17 09:01:33 -0400

    update import/export configuration

--- plugins/postcss-custom-properties/.rollup.js
+++ plugins/postcss-custom-properties/.rollup.js
@@ -1,10 +1,11 @@
 import babel from '@rollup/plugin-babel';
+import { copy } from '@web/rollup-plugin-copy'
 
 export default {
 	input: 'src/index.js',
 	output: [
-		{ file: 'index.cjs.js', format: 'cjs', sourcemap: true, exports: 'default' },
-		{ file: 'index.esm.mjs', format: 'esm', sourcemap: true, exports: 'default' }
+		{ file: 'index.cjs', format: 'cjs', exports: 'default' },
+		{ file: 'index.mjs', format: 'esm' }
 	],
 	plugins: [
 		babel({
@@ -17,10 +18,14 @@ export default {
 					corejs: 3,
 					loose: true,
 					modules: false,
-					targets: { node: 10 },
+					targets: { node: 12 },
 					useBuiltIns: 'entry'
 				}]
 			]
-		})
+		}),
+		copy({
+			rootDir: './src',
+			patterns: 'index.d.ts'
+		}),
 	]
 };
