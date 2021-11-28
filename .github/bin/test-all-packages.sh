ebe8acaa7fdb29a14f015e999e8d234542375035
romainmenke <romainmenke@gmail.com>
2021-11-28 23:47:19 +0100

    fix tests

--- .github/bin/test-all-packages.sh
+++ .github/bin/test-all-packages.sh
@@ -5,26 +5,23 @@ set -e
 start_dir=$(pwd)
 
 for package in $(ls -d ./packages/*); do
-	echo "Installing and testing $package"
+	echo "Testing $package"
 	cd $package
-	npm install --ignore-scripts
 	npm run test
 	cd $start_dir
 done
 
 
 for package in $(ls -d ./plugins/*); do
-	echo "Installing and testing $package"
+	echo "Testing $package"
 	cd $package
-	npm install --ignore-scripts
 	npm run test
 	cd $start_dir
 done
 
 for package in $(ls -d ./plugin-packs/*); do
-	echo "Installing and testing $package"
+	echo "Testing $package"
 	cd $package
-	npm install --ignore-scripts
 	npm run test
 	cd $start_dir
 done
