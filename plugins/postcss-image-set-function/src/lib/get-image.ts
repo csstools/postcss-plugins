6025e8e31217980400425e98930b2175f38d12cb
romainmenke <romainmenke@gmail.com>
2021-11-30 17:32:20 +0100

    improve readability

--- plugins/postcss-image-set-function/src/lib/get-image.ts
+++ plugins/postcss-image-set-function/src/lib/get-image.ts
@@ -5,15 +5,24 @@ const imageFuncRegexp = /^(cross-fade|image|(repeating-)?(conic|linear|radial)-g
 export function getImage(node) {
 	// <url> | <image()> | <cross-fade()> | <gradient>
 	// the image-set() function can not be nested inside of itself
-	return Object(node).type === 'func' &&
+	if (!node || !node.type) {
+		return false;
+	}
+
+	if (node.type === 'func' &&
 		imageFuncRegexp.test(node.name) &&
 		!(
+			node.parent &&
 			node.parent.parent &&
 			node.parent.parent.type === 'func' &&
 			imageSetFunctionMatchRegExp.test(node.parent.parent.name)
-		)
-		? (node.raws.before || '') + String(node)
-		: Object(node).type === 'quoted'
-			? node.value
-			: false;
+		)) {
+		return (node.raws.before || '') + String(node);
+	}
+
+	if (node.type === 'quoted') {
+		return node.value;
+	}
+
+	return false;
 }
