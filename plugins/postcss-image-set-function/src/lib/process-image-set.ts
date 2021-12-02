98b833aaf2582a7d7f7ee82772c844683696448a
romainmenke <romainmenke@gmail.com>
2021-12-02 14:06:09 +0100

    fix postcss-image-set-function with image lists

--- plugins/postcss-image-set-function/src/lib/process-image-set.ts
+++ plugins/postcss-image-set-function/src/lib/process-image-set.ts
@@ -4,48 +4,78 @@ import { getImage } from './get-image';
 import { getMedia, getMediaDPI } from './get-media';
 import { handleInvalidation } from './handle-invalidation';
 import type { AtRule, Container, Declaration, Result, Postcss } from 'postcss';
+import type { Node } from 'postcss-value-parser';
 
-export const processImageSet = (imageSetOptionNodes, decl: Declaration, opts: { decl: Declaration, oninvalid: string, preserve: boolean, result: Result, postcss: Postcss }) => {
+type imageSetFunction = {
+	imageSetFunction: Node;
+	imageSetOptionNodes: Array<Node>;
+}
+
+type mediaByDpr = {
+	atRule: AtRule;
+	value: string;
+}
+
+export const processImageSet = (imageSetFunctions: Array<imageSetFunction>, decl: Declaration, opts: { decl: Declaration, oninvalid: string, preserve: boolean, result: Result, postcss: Postcss }) => {
 	const parent = decl.parent;
-	const mediasByDpr: Map<number, AtRule> = new Map();
+	const mediasByDpr: Map<number, mediaByDpr> = new Map();
+	const declValue = decl.value;
 
-	const length = imageSetOptionNodes.length;
-	let index = -1;
+	for (let i = 0; i < imageSetFunctions.length; i++) {
+		const { imageSetFunction, imageSetOptionNodes } = imageSetFunctions[i];
+		const mediasByDprPerItem: Map<number, AtRule> = new Map();
 
-	while (index < length) {
-		const comma = index < 0 ? true : getComma(imageSetOptionNodes[index]);
-		const value = getImage(imageSetOptionNodes[index + 1]);
-		const mediaDPI = getMediaDPI(imageSetOptionNodes[index + 2]);
+		const length = imageSetOptionNodes.length;
+		let index = -1;
 
-		const media = getMedia(mediaDPI, opts.postcss);
+		while (index < length) {
+			const comma = index < 0 ? true : getComma(imageSetOptionNodes[index]);
+			const value = getImage(imageSetOptionNodes[index + 1]);
+			const mediaDPI = getMediaDPI(imageSetOptionNodes[index + 2]);
 
-		// handle invalidations
-		if (!comma) {
-			handleInvalidation(opts, 'expected a comma', valueParser.stringify(imageSetOptionNodes));
-			return;
-		} else if (!value) {
-			handleInvalidation(opts, 'unexpected image', valueParser.stringify(imageSetOptionNodes));
-			return;
-		} else if (!media || !mediaDPI || mediasByDpr.has(mediaDPI)) {
-			handleInvalidation(opts, 'unexpected resolution', valueParser.stringify(imageSetOptionNodes));
-			return;
+			const media = getMedia(mediaDPI, opts.postcss);
+
+			// handle invalidations
+			if (!comma) {
+				handleInvalidation(opts, 'expected a comma', valueParser.stringify(imageSetOptionNodes));
+				return;
+			} else if (!value) {
+				handleInvalidation(opts, 'unexpected image', valueParser.stringify(imageSetOptionNodes));
+				return;
+			} else if (!media || !mediaDPI || mediasByDprPerItem.has(mediaDPI)) {
+				handleInvalidation(opts, 'unexpected resolution', valueParser.stringify(imageSetOptionNodes));
+				return;
+			}
+
+			mediasByDprPerItem.set(mediaDPI, media);
+
+			if (mediasByDpr.has(mediaDPI)) {
+				const m = mediasByDpr.get(mediaDPI);
+				m.value = m.value.replace(valueParser.stringify(imageSetFunction), value.trim());
+				mediasByDpr.set(mediaDPI, m);
+			} else {
+				mediasByDpr.set(mediaDPI, {
+					atRule: media,
+					value: declValue.replace(valueParser.stringify(imageSetFunction), value.trim()),
+				});
+			}
+
+			index += 3;
 		}
+	}
 
-		mediasByDpr.set(mediaDPI, media);
-
+	for (const { atRule, value } of mediasByDpr.values()) {
 		// prepare @media { decl: <image> }
 		const parentClone = parent.clone().removeAll();
-		const declClone = decl.clone({ value: value.trim() });
+		const declClone = decl.clone({ value: value });
 
 		parentClone.append(declClone);
-		media.append(parentClone);
-
-		index += 3;
+		atRule.append(parentClone);
 	}
 
 	const medias = Array.from(mediasByDpr.keys())
 		.sort((a, b) => a - b)
-		.map(params => mediasByDpr.get(params));
+		.map(params => mediasByDpr.get(params).atRule);
 
 	if (!medias.length) {
 		return;
