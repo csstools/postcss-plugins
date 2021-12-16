8745a17763af585df687e4bf6ce1ad31abbac995
romainmenke <romainmenke@gmail.com>
2021-12-16 23:04:06 +0100

    wip

--- plugins/css-has-pseudo/src/index.js
+++ plugins/css-has-pseudo/src/index.js
@@ -65,7 +65,16 @@ creator.postcss = true;
 const defaultOptions = { preserve: true };
 
 /** Returns the string as an escaped CSS identifier. */
-const getEscapedCss = (/** @type {string} */ value) => encodeURIComponent(value).replace(/%3A/g, ':').replace(/%5B/g, '[').replace(/%5D/g, ']').replace(/%2C/g, ',').replace(/[():%[\],]/g, '\\$&');
+const getEscapedCss = (/** @type {string} */ value) => {
+	let x = encodeURIComponent(value);
+	x = x.replace(/%3A/g, ':');
+	x = x.replace(/%5B/g, '[');
+	x = x.replace(/%5D/g, ']');
+	x = x.replace(/%2C/g, ',');
+	x = x.replace(/[():%[\],]/g, '\\$&');
+
+	return x;
+};
 
 /** Returns whether the selector is within a `:not` pseudo-class. */
 const isParentInNotPseudo = (selector) => selector.parent?.parent?.type === 'pseudo' && selector.parent.parent.value === ':not';
