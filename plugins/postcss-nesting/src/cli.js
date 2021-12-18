f79c48fd7baa4e3e0182b12430daa44eb5f6f1e3
romainmenke <romainmenke@gmail.com>
2021-12-18 15:49:40 +0100

    add option to cli

--- plugins/postcss-nesting/src/cli.js
+++ plugins/postcss-nesting/src/cli.js
@@ -3,11 +3,13 @@ import { cli, helpTextLogger } from '@csstools/base-cli';
 
 cli(
 	plugin,
-	[],
+	['noIsPseudoSelector'],
 	helpTextLogger(
 		'postcss-nesting',
 		'PostCSS Nesting',
 		'Lets you nest style rules inside each other, following the CSS Nesting specification.',
-		null,
+		{
+			noIsPseudoSelector: true,
+		},
 	),
 );
