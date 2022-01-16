c7794cc7098ddc1b024825cf655d5a7d838d7ab7
Antonio Laguna <sombragriselros@gmail.com>
2022-01-16 17:37:53 +0100

    Adding useful logging to debug mode

--- plugin-packs/postcss-preset-env/src/lib/log-features-list.js
+++ plugin-packs/postcss-preset-env/src/lib/log-features-list.js
@@ -1,8 +1,10 @@
 import { log } from './log-helper';
+import { clientSideDocumentation } from './plugins-with-client-side';
 
 export default function logFeaturesList(supportedFeatures, options) {
 	if (options.debug) {
 		log('Enabling the following feature(s):');
+		const clientSideFeatures = [];
 
 		supportedFeatures.forEach(feature => {
 			if (feature.id.startsWith('before') || feature.id.startsWith('after')) {
@@ -10,6 +12,15 @@ export default function logFeaturesList(supportedFeatures, options) {
 			} else {
 				log(`  ${feature.id}`);
 			}
+
+			if (typeof clientSideDocumentation[feature.id] !== 'undefined') {
+				clientSideFeatures.push(feature.id);
+			}
 		});
+
+		if (clientSideFeatures.length) {
+			log('These feature(s) need a browser library to work:');
+			clientSideFeatures.forEach(featureId => log(` ${featureId}: ${clientSideDocumentation[featureId]}`));
+		}
 	}
 }
