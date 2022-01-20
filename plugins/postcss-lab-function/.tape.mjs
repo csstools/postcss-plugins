7e76ea8469f215e822dc7b627cef3c6126b2f9d3
romainmenke <romainmenke@gmail.com>
2022-01-20 17:03:24 +0100

    updates

--- plugins/postcss-lab-function/.tape.mjs
+++ plugins/postcss-lab-function/.tape.mjs
@@ -1,4 +1,7 @@
-module.exports = {
+import postcssTape from '../../packages/postcss-tape/dist/index.mjs';
+import plugin from 'postcss-lab-function';
+
+postcssTape(plugin)({
 	'basic': {
 		message: 'supports basic usage',
 	},
@@ -43,4 +46,4 @@ module.exports = {
 			preserve: true
 		}
 	}
-}
+});
