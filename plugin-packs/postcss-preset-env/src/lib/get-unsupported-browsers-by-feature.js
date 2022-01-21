21278bc87616f657c3899cba3f2ab70cdf5a2229
Antonio Laguna <sombragriselros@gmail.com>
2022-01-21 16:31:16 +0100

    Removing Can I Use Lite (#185)
    * Getting things up to date
    
    * Creating a query for browsers from given browser support
    
    * Updating tests
    
    * Moving to cssdb feature
    
    * Using the new logic with logs too
    
    * Allowing `.` for float versions
    
    * Updating package

--- plugin-packs/postcss-preset-env/src/lib/get-unsupported-browsers-by-feature.js
+++ plugin-packs/postcss-preset-env/src/lib/get-unsupported-browsers-by-feature.js
@@ -1,26 +1,41 @@
-import * as caniuse from 'caniuse-lite';
+const browsers = [
+	'ie',
+	'edge',
+	'firefox',
+	'chrome',
+	'safari',
+	'opera',
+	'ios_saf',
+	'android',
+	'op_mob',
+	'and_chr',
+	'and_ff',
+	'and_uc',
+	'samsung',
+	'and_qq',
+	'baidu',
+	'kaios',
+];
 
 // return a list of browsers that do not support the feature
 export default function getUnsupportedBrowsersByFeature(feature) {
-	const caniuseFeature = caniuse.features[feature];
-
 	// if feature support can be determined
-	if (caniuseFeature) {
-		const stats = caniuse.feature(caniuseFeature).stats;
+	if (feature.browser_support) {
+		const query = [];
 
-		// return an array of browsers and versions that do not support the feature
-		const results = Object.keys(stats).reduce(
-			(browsers, browser) => browsers.concat(
-				Object.keys(stats[browser]).filter(
-					version => stats[browser][version].indexOf('y') !== 0,
-				).map(
-					version => `${browser} ${version}`,
-				),
-			),
-			[],
-		);
+		browsers.forEach(browser => {
+			const browserSupport = feature.browser_support[browser];
+			// If the version is something like TP we can't do < TP
+			const isValid = typeof browserSupport === 'string' && browserSupport.match(/^[0-9|.]+$/) !== null;
 
-		return results;
+			if (isValid) {
+				query.push(`${browser} < ${feature.browser_support[browser]}`);
+			} else {
+				query.push(`${browser} >= 1`);
+			}
+		});
+
+		return query;
 	} else {
 		// otherwise, return that the feature does not work in any browser
 		return [ '> 0%' ];
