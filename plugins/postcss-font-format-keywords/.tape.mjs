fa19b8f1ff633632f55252125784f56b6dc88c06
Antonio Laguna <sombragriselros@gmail.com>
2022-01-23 13:15:43 +0100

    New plugin

---
+++ plugins/postcss-font-format-keywords/.tape.mjs
@@ -0,0 +1,20 @@
+import postcssTape from '../../packages/postcss-tape/dist/index.mjs';
+import plugin from '@csstools/postcss-font-format-keywords';
+
+postcssTape(plugin)({
+	'basic': {
+		message: 'supports basic usage',
+	},
+	'basic:preserve-true': {
+		message: 'supports { preserve: true } usage',
+		options: {
+			preserve: true
+		}
+	},
+	'basic:single-quotes': {
+		message: 'supports { singleQuotes: true } usage',
+		options: {
+			singleQuotes: true
+		}
+	},
+});
---
+++ plugins/postcss-font-format-keywords/src/index.ts
@@ -0,0 +1,70 @@
+import type { PluginCreator } from 'postcss';
+import valueParser, { StringNode } from 'postcss-value-parser';
+
+const keywords = [
+	'woff',
+	'truetype',
+	'opentype',
+	'woff2',
+	'embedded-opentype',
+	'collection',
+	'svg',
+];
+
+const creator: PluginCreator<{
+	preserve: boolean,
+	singleQuotes: boolean
+}> = (opts?: { preserve: boolean, singleQuotes: boolean }) => {
+	const preserve = 'preserve' in Object(opts) ? Boolean(opts.preserve) : false;
+	const singleQuotes = 'singleQuotes' in Object(opts) ? Boolean(opts.singleQuotes) : false;
+
+	return {
+		postcssPlugin: 'postcss-font-format-keywords',
+		AtRule: {
+			'font-face'(atRule) {
+				if (atRule.name !== 'font-face') {
+					// Case sensitive
+					return;
+				}
+
+				atRule.walkDecls('src', decl => {
+					if (!decl.value.includes('format(')) {
+						// No point on doing parsing if no format is specified
+						return;
+					}
+
+					const value = valueParser(decl.value);
+
+					value.walk((node) => {
+						if (node.type !== 'function' || node.value !== 'format') {
+							return;
+						}
+
+						node.nodes.forEach((child) => {
+							if (child.type !== 'word' || !keywords.includes(child.value)) {
+								return;
+							}
+
+							child.value = valueParser.stringify({
+								type: 'string',
+								value: child.value,
+								quote: singleQuotes ? '\'' : '"',
+							} as StringNode);
+						});
+					});
+
+					if (preserve) {
+						decl.cloneBefore({ value: value.toString() });
+					} else {
+						decl.value = value.toString();
+					}
+				});
+			},
+		},
+	};
+};
+
+creator.postcss = true;
+
+export default creator;
+
