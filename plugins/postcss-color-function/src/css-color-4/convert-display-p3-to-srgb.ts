e0b10d0f3c9d6022c5edc3dca157e9fecbdad218
romainmenke <romainmenke@gmail.com>
2022-02-04 17:36:03 +0100

    wip

--- plugins/postcss-color-function/src/css-color-4/convert-display-p3-to-srgb.ts
+++ plugins/postcss-color-function/src/css-color-4/convert-display-p3-to-srgb.ts
@@ -6,27 +6,16 @@ type color = [number, number, number];
 export function displayP3ToSRgb(displayP3: color): color {
 	let conversion = displayP3.slice() as color;
 
-	// convert an array of gamma-corrected display-p3 values
-	// in the 0.0 to 1.0 range
-	// to linear-light display-p3, then to CIE XYZ,
-	// then adapt from D65 to D50,
-	// then convert XYZ to CIE Lab
-	// and finally, convert to CIE LCH
-
-	// return Lab_to_LCH(XYZ_to_Lab(D65_to_D50(lin_P3_to_XYZ(lin_P3(RGB)))));
-
-	// https://drafts.csswg.org/css-color-4/#oklab-lab-to-predefined
+	// https://www.w3.org/TR/css-color-4/#predefined-to-predefined
+	// https://www.w3.org/TR/css-color-4/#predefined-to-lab-oklab
 	// 1. Convert Lab to(D50 - adapted) XYZ
 	conversion = lin_P3(conversion);
-	// conversion = D65_to_D50(conversion);
 	conversion = lin_P3_to_XYZ(conversion);
 
 	let oklch = conversion.slice() as color;
-	// oklch = D50_to_D65(oklch);
 	oklch = XYZ_to_OKLab(oklch);
 	oklch = OKLab_to_OKLCH(oklch);
 
-	// 2. If needed, convert from a D50 whitepoint(used by Lab) to the D65 whitepoint used in sRGB and most other RGB spaces, with the Bradford transform.prophoto - rgb' does not require this step.
 	// 3. Convert from(D65 - adapted) CIE XYZ to linear RGB
 	conversion = XYZ_to_lin_sRGB(conversion);
 	// 4. Convert from linear - light RGB to RGB(do gamma encoding)
