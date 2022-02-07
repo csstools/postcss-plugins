c06775410d052c62b3c8a89ff45a481e48619f67
Romain Menke <11521496+romainmenke@users.noreply.github.com>
2022-02-07 09:23:33 +0100

    postcss-color-function (#226)
    * postcss-color-function
    
    * lab on main does not output color func declarations yet
    
    * Update plugins/postcss-color-function/src/index.ts
    
    * fix @supports
    
    * Update plugins/postcss-color-function/src/index.ts
    
    * typo
    
    * fix
    
    * fixes
    
    * finish
    
    * fmt
    
    * wording

---
+++ plugins/postcss-color-function/.tape.mjs
@@ -0,0 +1,46 @@
+import postcssTape from '../../packages/postcss-tape/dist/index.mjs';
+import plugin from '@csstools/postcss-color-function';
+import lab from 'postcss-lab-function';
+
+postcssTape(plugin)({
+	'basic': {
+		message: 'supports basic usage',
+	},
+	'basic:preserve-true': {
+		message: 'supports { preserve: true } usage',
+		warnings: 6,
+		options: {
+			preserve: true
+		}
+	},
+	'lab-function-interop': {
+		message: 'supports usage along side postcss-lab-function',
+		plugins: [
+			plugin({ preserve: true }),
+			lab({ preserve: true, displayP3: true }),
+		]
+	},
+	'lab-function-interop:preserve:false': {
+		message: 'supports usage along side postcss-lab-function with { preserve: false }',
+		plugins: [
+			plugin({ preserve: false}),
+			lab({ preserve: false, displayP3: true }),
+		]
+	},
+	'variables': {
+		message: 'supports variables',
+	},
+	'variables:preserve-true': {
+		message: 'supports variables with { preserve: true } usage',
+		options: {
+			preserve: true
+		}
+	},
+	'variables:preserve-true:progressive-false': {
+		message: 'supports variables with { preserve: true, enableProgressiveCustomProperties: false } usage',
+		options: {
+			preserve: true,
+			enableProgressiveCustomProperties: false,
+		}
+	},
+});
---
+++ plugins/postcss-color-function/src/modified-value.ts
@@ -0,0 +1,40 @@
+import valueParser from 'postcss-value-parser';
+import type { ParsedValue, FunctionNode } from 'postcss-value-parser';
+import type { Declaration, Result } from 'postcss';
+import { onCSSFunctionSRgb } from './on-css-function';
+
+export function modifiedValues(originalValue: string, decl: Declaration, result: Result, preserve: boolean): string | undefined {
+	let valueASTSRgb: ParsedValue | undefined;
+
+	try {
+		valueASTSRgb = valueParser(originalValue);
+	} catch (error) {
+		decl.warn(
+			result,
+			`Failed to parse value '${originalValue}' as a color function. Leaving the original value intact.`,
+		);
+	}
+
+	if (typeof valueASTSRgb === 'undefined') {
+		return;
+	}
+
+	valueASTSRgb.walk((node) => {
+		if (!node.type || node.type !== 'function') {
+			return;
+		}
+
+		if (node.value !== 'color') {
+			return;
+		}
+
+		onCSSFunctionSRgb(node as FunctionNode, decl, result, preserve);
+	});
+	const modifiedValueSRgb = String(valueASTSRgb);
+
+	if (modifiedValueSRgb === originalValue) {
+		return;
+	}
+
+	return modifiedValueSRgb;
+}
