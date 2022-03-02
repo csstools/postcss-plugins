2ee990d6982e0eea0d4a14a6257aab77572a1d61
Antonio Laguna <sombragriselros@gmail.com>
2022-03-02 13:49:23 +0100

    Linting

--- plugin-packs/postcss-preset-env/src/patch/postcss-clamp.mjs
+++ plugin-packs/postcss-preset-env/src/patch/postcss-clamp.mjs
@@ -1,126 +1,126 @@
-const valueParser = require('postcss-value-parser')
+const valueParser = require('postcss-value-parser');
 
 function parseValue (value) {
-	let parsed = value.match(/([\d.-]+)(.*)/)
+	let parsed = value.match(/([\d.-]+)(.*)/);
 	if (!parsed || !parsed[1] || !parsed[2] || isNaN(parsed[1])) {
-		return undefined
+		return undefined;
 	}
-	return [parseFloat(parsed[1]), parsed[2]]
+	return [parseFloat(parsed[1]), parsed[2]];
 }
 
 function compose (first, second, third) {
 	if (first && second && third) {
-		return `max(${first}, min(${second}, ${third}))`
+		return `max(${first}, min(${second}, ${third}))`;
 	}
 	if (first && second) {
-		return `max(${first}, ${second})`
+		return `max(${first}, ${second})`;
 	}
 
-	return first
+	return first;
 }
 
 function updateValue (declaration, value, preserve) {
-	let newValue = value
-	let newValueAst = valueParser(value)
-	let valueAST = valueParser(declaration.value)
+	let newValue = value;
+	let newValueAst = valueParser(value);
+	let valueAST = valueParser(declaration.value);
 	// Walk can't be interrupted, so we only care about first
-	let foundClamp = false
+	let foundClamp = false;
 
 	valueAST.walk((node, index, nodes) => {
-		let isClamp = node.type === 'function' && node.value === 'clamp'
+		let isClamp = node.type === 'function' && node.value === 'clamp';
 
 		if (!isClamp || foundClamp) {
-			return
+			return;
 		}
 
-		foundClamp = true
-		nodes[index] = newValueAst
-	})
+		foundClamp = true;
+		nodes[index] = newValueAst;
+	});
 
 	if (foundClamp) {
-		newValue = valueAST.toString()
+		newValue = valueAST.toString();
 	}
 
 	if (preserve) {
-		declaration.cloneBefore({ value: newValue })
+		declaration.cloneBefore({ value: newValue });
 	} else {
-		declaration.value = newValue
+		declaration.value = newValue;
 	}
 }
 
 export default function postcssClamp(opts) {
-	opts = opts || {}
-	let precalculate = opts.precalculate ? Boolean(opts.precalculate) : false
-	let preserve = opts.preserve ? Boolean(opts.preserve) : false
+	opts = opts || {};
+	let precalculate = opts.precalculate ? Boolean(opts.precalculate) : false;
+	let preserve = opts.preserve ? Boolean(opts.preserve) : false;
 
 	return {
 		postcssPlugin: 'postcss-clamp',
 		Declaration (decl) {
 			if (!decl || !decl.value.includes('clamp')) {
-				return
+				return;
 			}
 			valueParser(decl.value).walk(node => {
-				let nodes = node.nodes
+				let nodes = node.nodes;
 				if (
 					node.type !== 'function' ||
 					node.value !== 'clamp' ||
 					nodes.length !== 5
 				) {
-					return
+					return;
 				}
-				let first = nodes[0]
-				let second = nodes[2]
-				let third = nodes[4]
+				let first = nodes[0];
+				let second = nodes[2];
+				let third = nodes[4];
 				let naive = compose(
 					valueParser.stringify(first),
 					valueParser.stringify(second),
-					valueParser.stringify(third)
-				)
+					valueParser.stringify(third),
+				);
 				if (!precalculate || second.type !== 'word' || third.type !== 'word') {
-					updateValue(decl, naive, preserve)
-					return
+					updateValue(decl, naive, preserve);
+					return;
 				}
-				let parsedSecond = parseValue(second.value)
-				let parsedThird = parseValue(third.value)
+				let parsedSecond = parseValue(second.value);
+				let parsedThird = parseValue(third.value);
 				if (parsedSecond === undefined || parsedThird === undefined) {
-					updateValue(decl, naive, preserve)
-					return
+					updateValue(decl, naive, preserve);
+					return;
 				}
-				let [secondValue, secondUnit] = parsedSecond
-				let [thirdValue, thirdUnit] = parsedThird
+				let [secondValue, secondUnit] = parsedSecond;
+				let [thirdValue, thirdUnit] = parsedThird;
 				if (secondUnit !== thirdUnit) {
-					updateValue(decl, naive, preserve)
-					return
+					updateValue(decl, naive, preserve);
+					return;
 				}
-				let parsedFirst = parseValue(first.value)
+				let parsedFirst = parseValue(first.value);
 				if (parsedFirst === undefined) {
-					let secondThirdValue = `${secondValue + thirdValue}${secondUnit}`
+					let secondThirdValue = `${secondValue + thirdValue}${secondUnit}`;
 					updateValue(
 						decl,
 						compose(valueParser.stringify(first), secondThirdValue),
-						preserve
-					)
-					return
+						preserve,
+					);
+					return;
 				}
-				let [firstValue, firstUnit] = parsedFirst
+				let [firstValue, firstUnit] = parsedFirst;
 				if (firstUnit !== secondUnit) {
-					let secondThirdValue = `${secondValue + thirdValue}${secondUnit}`
+					let secondThirdValue = `${secondValue + thirdValue}${secondUnit}`;
 					updateValue(
 						decl,
 						compose(valueParser.stringify(first), secondThirdValue),
-						preserve
-					)
-					return
+						preserve,
+					);
+					return;
 				}
 
 				updateValue(
 					decl,
 					compose(`${firstValue + secondValue + thirdValue}${secondUnit}`),
-					preserve
-				)
-			})
-		}
-	}
+					preserve,
+				);
+			});
+		},
+	};
 }
 
-postcssClamp.postcss = true
+postcssClamp.postcss = true;
