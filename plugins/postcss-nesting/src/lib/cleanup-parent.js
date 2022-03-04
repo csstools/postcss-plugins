85b9acece977858ec8c5612d985a7c84e5df34e3
romainmenke <romainmenke@gmail.com>
2022-03-04 18:57:52 +0100

    postcss-nesting : fix #285

--- plugins/postcss-nesting/src/lib/cleanup-parent.js
+++ plugins/postcss-nesting/src/lib/cleanup-parent.js
@@ -1,5 +1,11 @@
 export default function cleanupParent(parent) {
 	if (!parent.nodes.length) {
 		parent.remove();
+		return;
+	}
+
+	const commentNodes = parent.nodes.filter(node => node.type === 'comment');
+	if (commentNodes.length === parent.nodes.length) {
+		parent.replaceWith(...commentNodes);
 	}
 }
