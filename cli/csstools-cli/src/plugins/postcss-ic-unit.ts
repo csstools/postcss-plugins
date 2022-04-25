4a1203123a1a8c690724b676b6d19bb505184095
romainmenke <romainmenke@gmail.com>
2022-04-25 12:57:21 +0200

    update dependencies in postcss-preset-env and csstools-cli

--- cli/csstools-cli/src/plugins/postcss-ic-unit.ts
+++ cli/csstools-cli/src/plugins/postcss-ic-unit.ts
@@ -1,16 +1,17 @@
-import plugin from 'postcss-color-functional-notation';
+import plugin from '@csstools/postcss-ic-unit';
 import { cli, helpTextLogger } from '@csstools/base-cli';
 
 export default function postcssICUnit() {
 	cli(
 		plugin,
-		['preserve'],
+		['preserve', 'enableProgressiveCustomProperties'],
 		helpTextLogger(
 			'@csstools/cli postcss-ic-unit',
 			'PostCSS IC Unit',
 			'Lets you use the ic length unit.',
 			{
 				preserve: true,
+				enableProgressiveCustomProperties: false,
 			},
 		),
 		false,
