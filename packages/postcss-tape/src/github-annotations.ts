c79b509efdbe10a5b031ed18043d94f9afa7ea8d
romainmenke <romainmenke@gmail.com>
2022-05-01 10:05:21 +0200

    path fixes

--- packages/postcss-tape/src/github-annotations.ts
+++ packages/postcss-tape/src/github-annotations.ts
@@ -1,8 +1,17 @@
+import path from 'path';
+
 export function formatGitHubActionAnnotation(message, level = 'error', options = {}) {
 	let output = '::' + level;
 
 	const outputOptions = Object.keys(options).map((key) => {
-		return `${key}=${escapeValue(String(options[key]))}`;
+		let value = String(options[key]);
+
+		if (key === 'file' && process.env.GITHUB_WORKSPACE) {
+			// make file paths relative to the workspace root.
+			value = path.relative(process.env.GITHUB_WORKSPACE, path.resolve(value));
+		}
+
+		return `${key}=${escapeValue(value)}`;
 	}).join(',');
 
 	if (outputOptions) {
