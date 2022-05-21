d3d6cfca65aa9142339cd41700ae6a8a4636c00c
Antonio Laguna <sombragriselros@gmail.com>
2022-05-21 11:21:44 +0200

    Keeping track of changes

---
+++ packages/color-helpers/scripts/check-changes.mjs
@@ -0,0 +1,44 @@
+import crypto from 'crypto';
+import path from 'path';
+import { URL } from 'url';
+import fs from 'fs/promises';
+import fetch from 'node-fetch';
+
+const __dirname = new URL('.', import.meta.url).pathname;
+const hashesPath = path.resolve(__dirname, './hashes.json');
+const hashesFile = await fs.readFile(hashesPath, 'utf8').then(JSON.parse);
+const isRewriting = process.env.REWRITE_HASHES;
+
+const WATCHED_FILES = [
+	'https://raw.githubusercontent.com/w3c/csswg-drafts/main/css-color-4/utilities.js',
+	'https://raw.githubusercontent.com/w3c/csswg-drafts/main/css-color-4/conversions.js',
+	'https://raw.githubusercontent.com/w3c/csswg-drafts/main/css-color-4/deltaEOK.js',
+	'https://raw.githubusercontent.com/w3c/csswg-drafts/main/css-color-4/multiply-matrices.js',
+];
+
+const newHashes = await Promise.all(WATCHED_FILES.map(async urlPath => {
+	const content = await fetch(urlPath).then(response => response.text());
+	const hash = crypto.createHash('md5');
+	hash.update(content, 'utf8');
+	return hash.digest('hex');
+}));
+
+const changes = newHashes
+	.map((hash, index) => [hash, WATCHED_FILES[index]])
+	.filter(([hash], index) => hashesFile[index] !== hash)
+	.map(([,file]) => file);
+
+if (changes.length) {
+	console.log('Changes have been detected in the following files:');
+	changes.forEach(change => console.log('└ %s', change));
+
+	if (isRewriting) {
+		await fs.writeFile(hashesPath, JSON.stringify(newHashes));
+		console.log('Changes have been updated.');
+		process.exit(0);
+	} else {
+		process.exit(1);
+	}
+}
+
+console.log('No changes detected');
