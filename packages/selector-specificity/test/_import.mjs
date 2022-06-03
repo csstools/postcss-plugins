51e4a20a8da8235dbd17386a540a20db21bb02ba
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2022-06-04 00:04:42 +0900

    Add `compare()` function to `selector-specificity`

--- packages/selector-specificity/test/_import.mjs
+++ packages/selector-specificity/test/_import.mjs
@@ -1,6 +1,8 @@
 import assert from 'assert';
-import selectorSpecificity from '@csstools/selector-specificity';
+import selectorSpecificity, { compare } from '@csstools/selector-specificity';
 
 assert.equal(selectorSpecificity().a, 0);
 assert.equal(selectorSpecificity().b, 0);
 assert.equal(selectorSpecificity().c, 0);
+
+assert.equal(compare(selectorSpecificity(), selectorSpecificity()), 0);
