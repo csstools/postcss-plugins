c29a59f07b3172ff5f27da897ffeb257b6a7f838
Antonio Laguna <sombragriselros@gmail.com>
2022-06-11 12:57:04 +0200

    Revert "Getting creative"
    This reverts commit 3bcd1bd65855b4afd4a48698547a68b9b368d1d6.

--- rollup/default.js
+++ rollup/default.js
@@ -33,7 +33,7 @@ const presets = [];
 
 if (isTypescript) {
 	if (packageInfo.main || packageInfo.module) {
-		presets.push(...packageTypescript(packageInfo));
+		presets.push(...packageTypescript());
 	}
 
 	if (packageInfo.bin) {
--- rollup/presets/package-typescript.js
+++ rollup/presets/package-typescript.js
@@ -1,21 +1,16 @@
 import babel from '@rollup/plugin-babel';
 import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
-import path from 'path';
 import { packageBabelPreset } from '../configs/babel-presets';
 import { externalsForPlugin } from '../configs/externals';
 
-export function packageTypescript(pkg) {
-	return Object.keys(pkg.exports).map((exportKey) => {
-		const cjsFileName = pkg.exports[exportKey].require;
-		const mjsFileName = pkg.exports[exportKey].import;
-		const input = path.resolve('/src/', exportKey, './index.ts').slice(1);
-
-		return {
-			input,
+export function packageTypescript() {
+	return [
+		{
+			input: 'src/index.ts',
 			output: [
-				{ file: cjsFileName, format: 'cjs', sourcemap: false, exports: 'auto' },
-				{ file: mjsFileName, format: 'esm', sourcemap: false, exports: 'auto' },
+				{ file: 'dist/index.cjs', format: 'cjs', sourcemap: false, exports: 'auto' },
+				{ file: 'dist/index.mjs', format: 'esm', sourcemap: false, exports: 'auto' },
 			],
 			external: externalsForPlugin,
 			plugins: [
@@ -28,6 +23,6 @@ export function packageTypescript(pkg) {
 				}),
 				terser(),
 			],
-		};
-	});
+		},
+	];
 }
