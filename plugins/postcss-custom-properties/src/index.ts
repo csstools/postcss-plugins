88bb6bccb28436db3f51438ab154ce52e0a0da30
romainmenke <romainmenke@gmail.com>
2022-06-16 17:15:07 +0200

    please fail

--- plugins/postcss-custom-properties/src/index.ts
+++ plugins/postcss-custom-properties/src/index.ts
@@ -7,6 +7,9 @@ import transformProperties from './lib/transform-properties';
 import writeCustomPropertiesToExports from './lib/write-custom-properties-to-exports';
 import type { ImportOptions, ExportOptions } from './lib/options';
 
+/**
+ * @license YOU-CANT-USE-THIS
+ */
 export interface PluginOptions {
 	/** Do not emit warnings about "importFrom" and "exportTo" deprecations */
 	disableDeprecationNotice?: boolean;
