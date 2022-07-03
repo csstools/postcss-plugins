c4ba286a3d72f42ebf96f38a867dc2cfbbc0652d
romainmenke <romainmenke@gmail.com>
2022-07-03 18:53:29 +0200

    postcss-focus-within : browser compat

--- plugins/postcss-focus-within/src/browser.js
+++ plugins/postcss-focus-within/src/browser.js
@@ -1,4 +1,4 @@
-/* global document */
+/* global document,window */
 import isValidReplacement from './is-valid-replacement.mjs';
 function generateHandler(replaceWith) {
 	let selector;
@@ -65,7 +65,11 @@ export default function focusWithin(opts) {
 	const handleFocusChange = generateHandler(options.replaceWith);
 
 	const initialize = function initializeEventListeners() {
-		document.documentElement.classList.add('js-focus-within');
+		if (document.documentElement.className.indexOf('js-focus-within') > -1) {
+			return;
+		}
+
+		document.documentElement.className = document.documentElement.className + ' js-focus-within';
 		document.addEventListener('focus', handleFocusChange, true);
 		document.addEventListener('blur', handleFocusChange, true);
 	};
