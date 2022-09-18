0ee101bc7728e1971ee2fa42eb34e5c95fd2b765
romainmenke <romainmenke@gmail.com>
2022-09-18 12:35:21 +0200

    more tests

--- packages/css-tokenizer/test/consume/numeric-token.mjs
+++ packages/css-tokenizer/test/consume/numeric-token.mjs
@@ -21,3 +21,87 @@ import { Reader, consumeNumericToken } from '@csstools/css-tokenizer';
 		'10px',
 	);
 }
+
+{
+	const r = new Reader('10%');
+
+	const token = consumeNumericToken(r);
+
+	assert.deepEqual(
+		token,
+		['percentage', '10%', 0, 2, { value: 10 }],
+	);
+
+	assert.deepEqual(
+		r.slice(token[2], token[3] + 1),
+		'10%',
+	);
+
+	assert.deepEqual(
+		r.representationString(),
+		'10%',
+	);
+}
+
+{
+	const r = new Reader('-3.4e-2');
+
+	const token = consumeNumericToken(r);
+
+	assert.deepEqual(
+		token,
+		['number', '-3.4e-2', 0, 6, { value: -0.034, type: 'number' }],
+	);
+
+	assert.deepEqual(
+		r.slice(token[2], token[3] + 1),
+		'-3.4e-2',
+	);
+
+	assert.deepEqual(
+		r.representationString(),
+		'-3.4e-2',
+	);
+}
+
+{
+	const r = new Reader('-3.4e');
+
+	const token = consumeNumericToken(r);
+
+	assert.deepEqual(
+		token,
+		['dimension', '-3.4e', 0, 4, { value: -3.4, type: 'number', unit: 'e' }],
+	);
+
+	assert.deepEqual(
+		r.slice(token[2], token[3] + 1),
+		'-3.4e',
+	);
+
+	assert.deepEqual(
+		r.representationString(),
+		'-3.4e',
+	);
+}
+
+{
+	const r = new Reader('20--foo');
+
+	const token = consumeNumericToken(r);
+
+	assert.deepEqual(
+		token,
+		['dimension', '20--foo', 0, 6, { value: 20, type: 'integer', unit: '--foo' }],
+	);
+
+	assert.deepEqual(
+		r.slice(token[2], token[3] + 1),
+		'20--foo',
+	);
+
+	assert.deepEqual(
+		r.representationString(),
+		'20--foo',
+	);
+}
