604ca39a266a0a3690bc4594350e2d77c268c1d0
Romain Menke <romainmenke@gmail.com>
2022-11-27 11:49:22 +0100

    fix

--- rollup/presets/cli-javascript.js
+++ rollup/presets/cli-javascript.js
@@ -2,6 +2,7 @@ import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import path from 'path';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
 import { externalsForCLI } from '../configs/externals';
 import { addHashBang } from '../transforms/cli-hash-bang';
 import { packageBabelPreset } from '../configs/babel-presets';
@@ -24,6 +25,7 @@ export function cliJavascript() {
 					exclude: 'node_modules/**',
 					presets: packageBabelPreset,
 				}),
+				terser(),
 				addHashBang(),
 			],
 		},
--- rollup/presets/cli-typescript.js
+++ rollup/presets/cli-typescript.js
@@ -3,6 +3,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import path from 'path';
 import typescript from '@rollup/plugin-typescript';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
 import { externalsForCLI } from '../configs/externals';
 import { addHashBang } from '../transforms/cli-hash-bang';
 import { packageBabelPreset } from '../configs/babel-presets';
@@ -27,6 +28,7 @@ export function cliTypescript() {
 					extensions: ['.js', '.ts'],
 					presets: packageBabelPreset,
 				}),
+				terser(),
 				addHashBang(),
 			],
 		},
