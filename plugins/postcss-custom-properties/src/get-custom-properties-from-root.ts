cc425ee65813ec0e10d53e163c7622fbcb82e0d0
Romain Menke <romainmenke@gmail.com>
2022-12-03 19:16:50 +0100

    custom properties

--- plugins/postcss-custom-properties/src/get-custom-properties-from-root.ts
+++ plugins/postcss-custom-properties/src/get-custom-properties-from-root.ts
@@ -1,17 +1,24 @@
-import { Root, Rule } from 'postcss';
+import { Root } from 'postcss';
 import valuesParser from 'postcss-value-parser';
+import { cascadeLayerNumberForNode, collectCascadeLayerOrder } from './cascade-layers';
 import { isBlockIgnored, isDeclarationIgnored } from './is-ignored';
+import { isHtmlRule, isProcessableRule, isRootRule } from './is-processable-rule';
 
 // return custom selectors from the css root, conditionally removing them
 export default function getCustomPropertiesFromRoot(root: Root): Map<string, valuesParser.ParsedValue> {
 	// initialize custom selectors
-	const customPropertiesFromHtmlElement: Map<string, valuesParser.ParsedValue> = new Map();
-	const customPropertiesFromRootPseudo: Map<string, valuesParser.ParsedValue> = new Map();
-	const out: Map<string, valuesParser.ParsedValue> = new Map();
+	const customPropertiesFromHtmlElement: Map<string, string> = new Map();
+	const customPropertiesFromRootPseudo: Map<string, string> = new Map();
+	const customProperties: Map<string, string> = new Map();
+
+	const customPropertiesHtmlElementCascadeLayerMapping: Map<string, number> = new Map();
+	const customPropertiesRootPseudoCascadeLayerMapping: Map<string, number> = new Map();
+
+	const cascadeLayersOrder = collectCascadeLayerOrder(root);
 
 	// for each html or :root rule
-	root.each((rule) => {
-		if (rule.type !== 'rule') {
+	root.walkRules((rule) => {
+		if (!isProcessableRule(rule)) {
 			return;
 		}
 
@@ -33,7 +40,13 @@ export default function getCustomPropertiesFromRoot(root: Root): Map<string, val
 					return;
 				}
 
-				customPropertiesFromHtmlElement.set(decl.prop, valuesParser(decl.value));
+				const thisCascadeLayer = cascadeLayerNumberForNode(decl, cascadeLayersOrder);
+				const existingCascadeLayer = customPropertiesHtmlElementCascadeLayerMapping.get(decl.prop) ?? -1;
+
+				if (thisCascadeLayer >= existingCascadeLayer) {
+					customPropertiesHtmlElementCascadeLayerMapping.set(decl.prop, thisCascadeLayer);
+					customPropertiesFromHtmlElement.set(decl.prop, decl.value);
+				}
 			});
 		} else if (isRootRule(rule)) {
 			rule.each((decl) => {
@@ -49,31 +62,30 @@ export default function getCustomPropertiesFromRoot(root: Root): Map<string, val
 					return;
 				}
 
-				customPropertiesFromRootPseudo.set(decl.prop, valuesParser(decl.value));
+				const thisCascadeLayer = cascadeLayerNumberForNode(decl, cascadeLayersOrder);
+				const existingCascadeLayer = customPropertiesRootPseudoCascadeLayerMapping.get(decl.prop) ?? -1;
+
+				if (thisCascadeLayer >= existingCascadeLayer) {
+					customPropertiesRootPseudoCascadeLayerMapping.set(decl.prop, thisCascadeLayer);
+					customPropertiesFromRootPseudo.set(decl.prop, decl.value);
+				}
 			});
 		}
 	});
 
 	for (const [name, value] of customPropertiesFromHtmlElement.entries()) {
-		out.set(name, value);
+		customProperties.set(name, value);
 	}
 
 	for (const [name, value] of customPropertiesFromRootPseudo.entries()) {
-		out.set(name, value);
+		customProperties.set(name, value);
+	}
+
+	const out: Map<string, valuesParser.ParsedValue> = new Map();
+	for (const [name, value] of customProperties.entries()) {
+		out.set(name, valuesParser(value));
 	}
 
 	// return all custom properties, preferring :root properties over html properties
 	return out;
-}
-
-// match html and :root rules
-const htmlSelectorRegExp = /^html$/i;
-const rootSelectorRegExp = /^:root$/i;
-
-function isHtmlRule(rule: Rule) {
-	return rule.selector.split(',').some(item => htmlSelectorRegExp.test(item)) && rule.nodes && rule.nodes.length;
-}
-
-function isRootRule(rule: Rule) {
-	return rule.selector.split(',').some(item => rootSelectorRegExp.test(item)) && rule.nodes && rule.nodes.length;
 }
