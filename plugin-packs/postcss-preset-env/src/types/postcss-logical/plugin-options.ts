1c52e2b34bc360690059ec3e4e32f84120babfc0
Antonio Laguna <sombragriselros@gmail.com>
2023-01-22 12:36:35 +0100

    remove plugin-options for logical

--- plugin-packs/postcss-preset-env/src/types/postcss-logical/plugin-options.ts
+++
@@ -1,7 +0,0 @@
-/** postcss-logical plugin options */
-export type pluginOptions = {
-	/** Preserve the original notation. default: false */
-	preserve?: boolean,
-	/** Assume a direction for the document. default: null */
-	dir?: 'ltr' | 'rtl'
-};
