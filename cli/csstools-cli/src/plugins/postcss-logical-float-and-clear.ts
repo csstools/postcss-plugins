d2d04d2a3fbfc351baf4c5630a146f6b0278118e
Antonio Laguna <sombragriselros@gmail.com>
2023-01-22 16:49:31 +0100

    Updating CLI

---
+++ cli/csstools-cli/src/plugins/postcss-logical-float-and-clear.ts
@@ -0,0 +1,18 @@
+import plugin from '@csstools/postcss-logical-float-and-clear';
+import { cli, helpTextLogger } from '@csstools/base-cli';
+
+export default function postcssLogical() {
+	cli(
+		plugin,
+		['inlineDirection', 'blockDirection'],
+		helpTextLogger(
+			'@csstools/cli postcss-logical-float-and-clear',
+			'PostCSS Logical Float And Clear',
+			'Lets you use flow-relative (inline-start and inline-end) values for float and clear, following the CSS Logical Properties and Values specification.',
+			{
+				inlineDirection: 'left-to-right',
+			},
+		),
+		false,
+	);
+}
---
+++ cli/csstools-cli/src/plugins/postcss-logical-resize.ts
@@ -0,0 +1,18 @@
+import plugin from '@csstools/postcss-logical-resize';
+import { cli, helpTextLogger } from '@csstools/base-cli';
+
+export default function postcssLogicalResize() {
+	cli(
+		plugin,
+		['inlineDirection'],
+		helpTextLogger(
+			'@csstools/cli postcss-logical-resize',
+			'PostCSS Logical Resize',
+			'Lets you use logical values in the resize property, following the CSS Logical Properties and Values specification.',
+			{
+				inlineDirection: 'left-to-right',
+			},
+		),
+		false,
+	);
+}
