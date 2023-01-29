eebc81b6ce3c75976af8fb8018d67d213ec22ad3
Romain Menke <romainmenke@gmail.com>
2023-01-29 18:50:05 +0100

    remove license from code that was written by me

--- packages/color-helpers/src/calculations/binary-search-gamut.ts
+++ packages/color-helpers/src/calculations/binary-search-gamut.ts
@@ -2,11 +2,6 @@ import { clip } from 'utils/clip';
 import { OKLCH_to_OKLab } from 'conversions/oklch-to-oklab';
 import { deltaEOK } from 'calculations/delta-EOK';
 
-/**
- * @license W3C https://www.w3.org/Consortium/Legal/2015/copyright-software-and-document
- *
- * @copyright This software or document includes material copied from or derived from https://github.com/w3c/csswg-drafts/blob/main/css-color-4/map-gamut.js. Copyright © 2022 W3C® (MIT, ERCIM, Keio, Beihang).
- */
 export function binarySearchGamut(
 	startOKLCH: Color,
 	toDestination: (x: Color) => Color,
