4b718f42936f7848bc9733bda7196ac29004a86c
Romain Menke <romainmenke@gmail.com>
2023-01-29 10:48:01 +0100

    wip

--- sites/postcss-preset-env/src/static/js/playground.js
+++ sites/postcss-preset-env/src/static/js/playground.js
@@ -10,9 +10,11 @@ import postcssPresetEnv from 'postcss-preset-env';
 // hack to make browserslist work in a browser. (irony...)
 self.process = { env: {} };
 
-const startConfig = {
+const currentConfig = {
 	browsers: ['last 5 versions', 'not dead'],
-	stage: 0,
+	minimumVendorImplementations: 0,
+	stage: 2,
+	preserve: null,
 };
 
 function processCss(source, config) {
@@ -30,7 +32,31 @@ function processCss(source, config) {
 }
 
 function renderConfig(config) {
-	return JSON.stringify(config, null, '\t').replaceAll('\n', '\n\t\t');
+	const copy = Object.assign({}, config);
+	if (!copy.preserve) {
+		delete copy.preserve;
+	}
+
+	if (copy.stage === 2) {
+		delete copy.stage;
+	}
+
+	if (copy.minimumVendorImplementations === 0) {
+		delete copy.minimumVendorImplementations;
+	}
+
+	if (copy.browsers.length === 0) {
+		delete copy.browsers;
+	}
+
+	return `const postcssPresetEnv = require('postcss-preset-env');
+
+module.exports = {
+	plugins: [
+		postcssPresetEnv(${JSON.stringify(copy, null, '\t').replaceAll('\n', '\n\t\t')})
+	]
+}
+`;
 }
 
 let inputState = EditorState.create({
@@ -46,7 +72,7 @@ a {
 	color: rgb(0 0 100% / 90%);
 
 	&:hover {
-		color: rebeccapurple;
+		color: oklch(40% 0.268735435 34.568626);
 	}
 
 	& span {
@@ -58,7 +84,7 @@ a {
 
 @media (--tablet) {
 	:is(input, button):is(:hover, :focus) {
-		color: oklch(40% 0.268735435 34.568626);
+		color: rebeccapurple;
 	}
 }
 `,
@@ -80,14 +106,7 @@ let outputState = EditorState.create({
 });
 
 let configState = EditorState.create({
-	doc: `const postcssPresetEnv = require('postcss-preset-env');
-
-module.exports = {
-	plugins: [
-		postcssPresetEnv(${renderConfig(startConfig)})
-	]
-}
-`,
+	doc: renderConfig(currentConfig),
 	extensions: [
 		basicSetup,
 		javascript(),
@@ -107,7 +126,7 @@ function syncDispatch(tr, view, other) {
 			annotations.push(Transaction.userEvent.of(userEvent));
 		}
 
-		processCss(view.state.doc, startConfig).then((output) => {
+		processCss(view.state.doc, currentConfig).then((output) => {
 			let update = other.state.update({ changes: { from: 0, to: other.state.doc.length, insert: output } });
 			other.update([update]);
 		});
@@ -130,7 +149,49 @@ let outputView = new EditorView({
 	parent: document.querySelector('#output-editor'),
 });
 
-processCss(inputState.doc, startConfig).then((output) => {
+processCss(inputState.doc, currentConfig).then((output) => {
 	let update = outputView.state.update({ changes: { from: 0, to: outputView.state.doc.length, insert: output } });
 	outputView.update([update]);
 });
+
+let controls = {
+	browsers: document.getElementById('browsers'),
+	minimumVendorImplementations: document.getElementById('minimumVendorImplementations'),
+	stage: document.getElementById('stage'),
+	preserve: document.getElementById('preserve'),
+};
+
+controls.browsers.value = currentConfig.browsers.join(', ');
+controls.minimumVendorImplementations.value = currentConfig.minimumVendorImplementations.toString();
+controls.stage.value = currentConfig.stage.toString();
+if (currentConfig.preserve === true) {
+	controls.preserve.value = 'true';
+} else if (currentConfig.preserve === false) {
+	controls.preserve.value = 'false';
+}
+
+for (const control of Object.values(controls)) {
+	control.addEventListener('change', () => {
+		let preserve = null;
+		if (controls.preserve.value === 'true') {
+			preserve = true;
+		} else if (controls.preserve.value === 'false') {
+			preserve = false;
+		}
+
+		currentConfig.browsers = controls.browsers.value.split(',').filter((x) => !!x);
+		currentConfig.minimumVendorImplementations = parseInt(controls.minimumVendorImplementations.value || '0', 10);
+		currentConfig.stage = parseInt(controls.stage.value || '0', 10);
+		currentConfig.preserve = preserve;
+
+		processCss(inputState.doc, currentConfig).then((output) => {
+			configView.update([
+				configView.state.update({ changes: { from: 0, to: configView.state.doc.length, insert: renderConfig(currentConfig) } }),
+			]);
+
+			outputView.update([
+				outputView.state.update({ changes: { from: 0, to: outputView.state.doc.length, insert: output } }),
+			]);
+		});
+	});
+}
