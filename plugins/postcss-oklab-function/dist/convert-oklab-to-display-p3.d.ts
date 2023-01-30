574534896931d635aca8e1199e0a7d2349f53034
Romain Menke <romainmenke@gmail.com>
2023-01-30 18:53:28 +0100

    one more plugin

--- plugins/postcss-oklab-function/dist/convert-oklab-to-display-p3.d.ts
+++
@@ -1,3 +0,0 @@
-type color = [number, number, number];
-export declare function oklabToDisplayP3(oklabRaw: color): [color, boolean];
-export {};
--- plugins/postcss-oklab-function/dist/convert-oklch-to-srgb.d.ts
+++
@@ -1,3 +0,0 @@
-type color = [number, number, number];
-export declare function oklchToSRgb(oklchRaw: color): color;
-export {};
