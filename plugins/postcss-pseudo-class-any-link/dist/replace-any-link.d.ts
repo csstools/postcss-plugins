6aed4ee337a755c202807a6201174abb3a94a0b8
Romain Menke <romainmenke@gmail.com>
2023-02-08 19:39:45 +0100

    postcss-pseudo-class-any-link

--- plugins/postcss-pseudo-class-any-link/dist/replace-any-link.d.ts
+++ plugins/postcss-pseudo-class-any-link/dist/replace-any-link.d.ts
@@ -1,2 +1,2 @@
 import type { Result, Rule } from 'postcss';
-export declare function replaceAnyLink(rule: Rule, result: Result, preserve: boolean, areaHrefNeedsFixing: boolean): void;
+export declare function replaceAnyLink(rule: Rule, result: Result, preserve: boolean, areaHrefNeedsFixing: boolean): boolean;
