718c02d64202b0539734295aff5a811a3348bfc9
Romain Menke <11521496+romainmenke@users.noreply.github.com>
2023-02-13 13:52:30 +0100

    postcss-nesting : ampersand everywhere (#853)

---
+++ plugins/postcss-nesting/src/lib/ampersand-to-scope.ts
@@ -0,0 +1,42 @@
+import type { Container, Node, Result, Rule } from 'postcss';
+import parser from 'postcss-selector-parser';
+import { sortCompoundSelectorsInsideComplexSelector } from './merge-selectors/compound-selector-order';
+
+export default function ampersandToScope(rule: Rule, result: Result) {
+	let parent: Container<Node> = rule.parent;
+
+	while (parent) {
+		if (parent.type === 'rule') {
+			// Skip any rules that nested.
+			// We only want to process "&" found in unnested rules.
+			return;
+		}
+
+		parent = parent.parent;
+	}
+
+	let selectorAST: parser.Root;
+	try {
+		selectorAST = parser().astSync(rule.selector);
+	} catch (err) {
+		rule.warn(result, `Failed to parse selector : "${rule.selector}" with message: "${err.message}"`);
+		return;
+	}
+	if (!selectorAST) {
+		return;
+	}
+
+	selectorAST.walkNesting((nesting) => {
+		const parentNode = nesting.parent;
+
+		nesting.replaceWith(parser.pseudo({
+			value: ':scope',
+		}));
+
+		if (parentNode) {
+			sortCompoundSelectorsInsideComplexSelector(parentNode);
+		}
+	});
+
+	rule.selector = selectorAST.toString();
+}
