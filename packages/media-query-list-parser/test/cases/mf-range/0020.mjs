258c4ffdd3574022596d46e332ce67ebdb606790
Romain Menke <11521496+romainmenke@users.noreply.github.com>
2023-06-21 09:48:12 +0200

    media queries : fix parsing of range queries with double equal signs (#1006)

---
+++ packages/media-query-list-parser/test/cases/mf-range/0020.mjs
@@ -0,0 +1,14 @@
+import assert from 'assert';
+import { runTest } from '../../util/run-test.mjs';
+
+runTest(
+	'(width == 50px)',
+	'mf-range/0020',
+	(actual, expected) => {
+		assert.deepStrictEqual(
+			actual,
+			expected,
+		);
+	},
+	1,
+);
