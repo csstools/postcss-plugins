edb9ce1e5b23a8ee4f9256d08150f2168d58f8ab
Romain Menke <11521496+romainmenke@users.noreply.github.com>
2023-07-11 19:22:39 +0200

    fix browserslist support queries (#1042)

---
+++ plugin-packs/postcss-preset-env/src/lib/browsers-with-supports-stats.mjs
@@ -0,0 +1,16 @@
+// Matches the browsers that have support stats in cssdb.
+export const browsersWithSupportStats = [
+	'and_chr',
+	'and_ff',
+	'android',
+	'chrome',
+	'edge',
+	'firefox',
+	'ie',
+	'ios_saf',
+	'op_mini',
+	'op_mob',
+	'opera',
+	'safari',
+	'samsung',
+];
