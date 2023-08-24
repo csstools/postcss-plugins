4924b2d96e8cf7ba965d2c181d0e65c6ae56de38
Romain Menke <11521496+romainmenke@users.noreply.github.com>
2023-08-24 18:20:20 +0200

    `postcss-bundler` : extra tests (#1060)

--- plugin-packs/postcss-bundler/.tape.mjs
+++ plugin-packs/postcss-bundler/.tape.mjs
@@ -1,5 +1,7 @@
 import { postcssTape } from '@csstools/postcss-tape';
 import plugin from '@csstools/postcss-bundler';
+import fs from 'fs/promises';
+import path from 'path';
 
 const testCases = {
 	basic: {
@@ -17,5 +19,61 @@ const testCases = {
 		message: 'minimal example',
 	},
 }
+
+;(await fs.readdir('./test/css-import-tests', { withFileTypes: true, recursive: true })).filter(dirent => {
+	return dirent.isFile() && dirent.name === 'style.css'
+}).sort().forEach(dirent => {
+	const key = path.join(path.relative('./test', dirent.path), 'style');
+	testCases[key] = {
+		message: `passes 'css-import-tests' case : ${path.relative('./test/css-import-tests', dirent.path).split(path.sep).join(' - ')}`,
+	}
+
+	switch (dirent.path.split(path.sep).join(path.posix.sep)) {
+		case 'test/css-import-tests/003-should-fail/001-core-features/before-other-styles/002':
+			testCases[key].warnings = 1;
+			break;
+		case 'test/css-import-tests/002-sub-features/004-at-supports/007':
+			testCases[key].warnings = 1;
+			break;
+		case 'test/css-import-tests/002-sub-features/003-at-layer/011':
+			testCases[key].warnings = 1;
+			break;
+		case 'test/css-import-tests/002-sub-features/001-data-urls/004':
+			testCases[key].warnings = 1;
+			break;
+		case 'test/css-import-tests/001-core-features/namespace/002':
+			testCases[key].warnings = 1;
+			break;
+		case 'test/css-import-tests/001-core-features/before-other-styles/001':
+			testCases[key].warnings = 1;
+			break;
+		case 'test/css-import-tests/001-core-features/before-other-styles/002':
+			testCases[key].warnings = 1;
+			break;
+		case 'test/css-import-tests/001-core-features/url-fragments/004':
+			testCases[key].warnings = 1;
+			break;
+		default:
+			break;
+	}
+
+	switch (dirent.path.split(path.sep).join(path.posix.sep)) {
+		case 'test/css-import-tests/003-should-fail/001-core-features/before-other-styles/001':
+			testCases[key].exception = /At-rule without name/;
+			break;
+		case 'test/css-import-tests/005-implementation-specific/leading-slash-is-import-root/001':
+			testCases[key].exception = /Failed to find '\/a.css'/;
+			break;
+		case 'test/css-import-tests/005-implementation-specific/leading-slash-is-import-root/002':
+			testCases[key].exception = /Failed to find '\/b.css'/;
+			break;
+		case 'test/css-import-tests/999-irrelevant/url-format/002':
+			testCases[key].exception = /Unclosed string/;
+			break;
+
+		default:
+			break;
+	}
+});
 
 postcssTape(plugin)(testCases);
