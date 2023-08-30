402d9e99accd5392927feddbed912dc62260a6ca
Romain Menke <romainmenke@gmail.com>
2023-08-30 23:24:00 +0200

    stick with wcag

--- packages/css-color-parser/src/functions/contrast-color.ts
+++ packages/css-color-parser/src/functions/contrast-color.ts
@@ -2,12 +2,9 @@ import type { ColorData } from '../color-data';
 import type { ColorParser } from '../color-parser';
 import type { FunctionNode } from '@csstools/css-parser-algorithms';
 import { ColorNotation } from '../color-notation';
-import { SyntaxFlag, colorDataTo, colorData_to_XYZ_D50 } from '../color-data';
+import { SyntaxFlag, colorData_to_XYZ_D50 } from '../color-data';
 import { isCommentNode, isWhitespaceNode } from '@csstools/css-parser-algorithms';
-import { apca_adobeRGBtoY, apca_contrast, apca_displayP3toY, apca_sRGBtoY } from '../util/apca';
 import { Color } from '@csstools/color-helpers';
-import { XYZ_D50_to_P3_Gamut } from '../gamut-mapping/p3';
-import { XYZ_D50_to_a98_Gamut } from '../gamut-mapping/a98';
 import { XYZ_D50_to_sRGB_Gamut } from '../gamut-mapping/srgb';
 
 export function contrastColor(colorMixNode: FunctionNode, colorParser: ColorParser): ColorData | false {
@@ -36,97 +33,47 @@ export function contrastColor(colorMixNode: FunctionNode, colorParser: ColorPars
 		return false;
 	}
 
-	// 1. Convert to OKLCH
-	// 2. Resolve all missing and powerless components to zero
-	// 3. Above 0.72 L (lightness) use black, below use white
-
-	// Convert to APCA supported color spaces
-	switch (backgroundColor.colorNotation) {
-		case ColorNotation.HEX:
-		case ColorNotation.RGB:
-		case ColorNotation.sRGB:
-			backgroundColor = colorDataTo(backgroundColor, ColorNotation.sRGB);
-			break;
-		case ColorNotation.Display_P3:
-			backgroundColor = colorDataTo(backgroundColor, ColorNotation.Display_P3);
-			break;
-		case ColorNotation.A98_RGB:
-			backgroundColor = colorDataTo(backgroundColor, ColorNotation.A98_RGB);
-			break;
-		case ColorNotation.HSL:
-			backgroundColor = colorDataTo(backgroundColor, ColorNotation.sRGB);
-			break;
-		default:
-			backgroundColor = colorDataTo(backgroundColor, ColorNotation.A98_RGB);
-			break;
-	}
-
 	// Treat missing as zero.
 	backgroundColor.channels = backgroundColor.channels.map((x) => Number.isNaN(x) ? 0 : x) as Color;
+	backgroundColor.channels = XYZ_D50_to_sRGB_Gamut(colorData_to_XYZ_D50(backgroundColor).channels);
+	backgroundColor.colorNotation = ColorNotation.sRGB;
 
-	// Calculate contrast color
-	switch (backgroundColor.colorNotation) {
-		case ColorNotation.HEX:
-		case ColorNotation.RGB:
-		case ColorNotation.sRGB: {
-			const backgroundY = apca_sRGBtoY(XYZ_D50_to_sRGB_Gamut(colorData_to_XYZ_D50(backgroundColor).channels));
+	const colorData: ColorData = {
+		colorNotation: ColorNotation.sRGB,
+		channels: [0, 0, 0],
+		alpha: 1,
+		syntaxFlags: new Set([SyntaxFlag.ContrastColor, SyntaxFlag.Experimental]),
+	};
 
-			const whiteContrast = Math.abs(apca_contrast(1, backgroundY));
-			const blackContrast = Math.abs(apca_contrast(0, backgroundY));
-
-			const colorData: ColorData = {
-				colorNotation: ColorNotation.sRGB,
-				channels: [0, 0, 0],
-				alpha: 1,
-				syntaxFlags: new Set([SyntaxFlag.ContrastColor, SyntaxFlag.Experimental]),
-			};
-
-			if (whiteContrast > blackContrast) {
-				colorData.channels = [1, 1, 1];
-			}
-
-			return colorData;
-		}
-		case ColorNotation.Display_P3: {
-			const backgroundY = apca_displayP3toY(XYZ_D50_to_P3_Gamut(colorData_to_XYZ_D50(backgroundColor).channels));
-
-			const whiteContrast = Math.abs(apca_contrast(1, backgroundY));
-			const blackContrast = Math.abs(apca_contrast(0, backgroundY));
-
-			const colorData: ColorData = {
-				colorNotation: ColorNotation.Display_P3,
-				channels: [0, 0, 0],
-				alpha: 1,
-				syntaxFlags: new Set([SyntaxFlag.ContrastColor, SyntaxFlag.Experimental]),
-			};
-
-			if (whiteContrast > blackContrast) {
-				colorData.channels = [1, 1, 1];
-			}
-
-			return colorData;
-		}
-		case ColorNotation.A98_RGB: {
-			const backgroundY = apca_adobeRGBtoY(XYZ_D50_to_a98_Gamut(colorData_to_XYZ_D50(backgroundColor).channels));
-
-			const whiteContrast = Math.abs(apca_contrast(1, backgroundY));
-			const blackContrast = Math.abs(apca_contrast(0, backgroundY));
-
-			const colorData: ColorData = {
-				colorNotation: ColorNotation.A98_RGB,
-				channels: [0, 0, 0],
-				alpha: 1,
-				syntaxFlags: new Set([SyntaxFlag.ContrastColor, SyntaxFlag.Experimental]),
-			};
-
-			if (whiteContrast > blackContrast) {
-				colorData.channels = [1, 1, 1];
-			}
-
-			return colorData;
-		}
-		default:
-			// Should be unreachable
-			return false;
+	const contrastWhite = contrastRatio(backgroundColor.channels, [1, 1, 1]);
+	const contrastBlack = contrastRatio(backgroundColor.channels, [0, 0, 0]);
+	if (contrastWhite > contrastBlack) {
+		colorData.channels = [1, 1, 1];
+	} else {
+		colorData.channels = [0, 0, 0];
 	}
+
+	return colorData;
+}
+
+function luminance(color: Color): number {
+	// https://www.w3.org/TR/WCAG21/#dfn-relative-luminance
+	const [lumR, lumG, lumB] = color.map(component => {
+		return component <= 0.03928
+			? component / 12.92
+			: Math.pow((component + 0.055) / 1.055, 2.4);
+	});
+
+	return 0.2126 * lumR + 0.7152 * lumG + 0.0722 * lumB;
+}
+
+function contrastRatio(color1: Color, color2: Color): number {
+	// https://www.w3.org/TR/WCAG21/#dfn-contrast-ratio
+	const color1luminance = luminance(color1);
+	const color2luminance = luminance(color2);
+
+	const l1 = Math.max(color1luminance, color2luminance);
+	const l2 = Math.min(color1luminance, color2luminance);
+
+	return (l1 + 0.05) / (l2 + 0.05);
 }
