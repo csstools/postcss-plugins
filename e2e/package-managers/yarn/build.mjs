c19066212c4cbcf33685d2fc7aa5467b3b2281df
Romain Menke <romainmenke@gmail.com>
2023-09-13 10:58:30 +0200

    a better test

--- e2e/package-managers/yarn/build.mjs
+++
@@ -1,6 +0,0 @@
-import postcssPresetEnv from 'postcss-preset-env';
-import postcss from 'postcss';
-
-const result = await postcss([postcssPresetEnv({ browsers: 'last 100 Safari versions' })]).process('a { color: oklch(50% 50% 50deg); }', { from: undefined });
-
-console.log(result.css);
