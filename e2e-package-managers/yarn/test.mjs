b14095b98ba7bea8a14f935c58988ab9cbb9898d
Romain Menke <romainmenke@gmail.com>
2023-10-09 19:49:32 +0200

    simplify test

--- e2e-package-managers/yarn/test.mjs
+++ e2e-package-managers/yarn/test.mjs
@@ -2,6 +2,6 @@ import postcssPresetEnv from 'postcss-preset-env';
 import postcss from 'postcss';
 import assert from 'assert';
 
-const result = await postcss([postcssPresetEnv({ browsers: 'ie 11' })]).process('a { color: oklch(50% 50% 50deg); }', { from: undefined });
+const result = await postcss([postcssPresetEnv({ browsers: 'ie 11' })]).process('a { color: oklch(50% 1% 50deg); }', { from: undefined });
 
-assert.strictEqual(result.css, 'a { color: rgb(163, 65, 0); color: color(display-p3 0.60751 0.2644 0); }');
+assert.strictEqual(result.css, 'a { color: rgb(101, 99, 97); }');
