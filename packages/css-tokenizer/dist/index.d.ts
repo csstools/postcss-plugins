86abc117a43adf548f71dab4eb770c2396c4ae6b
Romain Menke <romainmenke@gmail.com>
2023-11-05 20:01:54 +0100

    stricter type re-exports

--- packages/css-tokenizer/dist/index.d.ts
+++ packages/css-tokenizer/dist/index.d.ts
@@ -1,9 +1,10 @@
 export type { CSSToken } from './interfaces/token';
+export type { Token } from './interfaces/token';
 export { ParseError } from './interfaces/error';
 export { Reader } from './reader';
-export { Token, TokenType, NumberType, mirrorVariantType, mirrorVariant, isToken } from './interfaces/token';
+export { TokenType, NumberType, mirrorVariantType, mirrorVariant, isToken } from './interfaces/token';
+export { cloneTokens } from './util/clone-tokens';
 export { stringify } from './stringify';
 export { tokenize, tokenizer } from './tokenizer';
-export { cloneTokens } from './util/clone-tokens';
 export type { TokenAtKeyword, TokenBadString, TokenBadURL, TokenCDC, TokenCDO, TokenColon, TokenComma, TokenComment, TokenDelim, TokenDimension, TokenEOF, TokenFunction, TokenHash, TokenIdent, TokenNumber, TokenPercentage, TokenSemicolon, TokenString, TokenURL, TokenWhitespace, TokenOpenParen, TokenCloseParen, TokenOpenSquare, TokenCloseSquare, TokenOpenCurly, TokenCloseCurly, } from './interfaces/token';
 export { mutateIdent, } from './util/mutations';
--- packages/media-query-list-parser/src/index.ts
+++ packages/media-query-list-parser/src/index.ts
@@ -27,23 +27,26 @@ export {
 	isMediaQueryWithoutType,
 } from './util/type-predicates';
 
+export type { MediaConditionList } from './nodes/media-condition-list';
+export type { MediaFeatureComparison } from './nodes/media-feature-comparison';
+export type { MediaFeatureRange } from './nodes/media-feature-range';
+export type { MediaQuery } from './nodes/media-query';
 export { CustomMedia } from './nodes/custom-media';
 export { GeneralEnclosed } from './nodes/general-enclosed';
 export { MediaAnd } from './nodes/media-and';
 export { MediaCondition } from './nodes/media-condition';
-export { MediaConditionList, MediaConditionListWithAnd, MediaConditionListWithOr } from './nodes/media-condition-list';
+export { MediaConditionListWithAnd, MediaConditionListWithOr } from './nodes/media-condition-list';
 export { MediaFeature, newMediaFeatureBoolean, newMediaFeaturePlain } from './nodes/media-feature';
 export { MediaFeatureBoolean } from './nodes/media-feature-boolean';
+export { MediaFeatureEQ, MediaFeatureGT, MediaFeatureLT, invertComparison, matchesComparison, comparisonFromTokens } from './nodes/media-feature-comparison';
 export { MediaFeatureName } from './nodes/media-feature-name';
 export { MediaFeaturePlain } from './nodes/media-feature-plain';
-export { MediaFeatureRange, MediaFeatureRangeNameValue, MediaFeatureRangeValueName, MediaFeatureRangeValueNameValue } from './nodes/media-feature-range';
+export { MediaFeatureRangeNameValue, MediaFeatureRangeValueName, MediaFeatureRangeValueNameValue } from './nodes/media-feature-range';
 export { MediaFeatureValue, matchesRatio, matchesRatioExactly } from './nodes/media-feature-value';
 export { MediaInParens } from './nodes/media-in-parens';
 export { MediaNot } from './nodes/media-not';
 export { MediaOr  } from './nodes/media-or';
-export { MediaQuery, MediaQueryWithType, MediaQueryWithoutType, MediaQueryInvalid } from './nodes/media-query';
-
-export { MediaFeatureComparison, MediaFeatureEQ, MediaFeatureGT, MediaFeatureLT, invertComparison, matchesComparison, comparisonFromTokens } from './nodes/media-feature-comparison';
-export { MediaType, typeFromToken } from './nodes/media-type';
 export { MediaQueryModifier, modifierFromToken } from './nodes/media-query-modifier';
+export { MediaQueryWithType, MediaQueryWithoutType, MediaQueryInvalid } from './nodes/media-query';
+export { MediaType, typeFromToken } from './nodes/media-type';
 export { cloneMediaQuery } from './util/clone-media-query';
