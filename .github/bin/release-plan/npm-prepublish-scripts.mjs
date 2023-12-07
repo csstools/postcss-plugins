1eee460fa18efa5a7d7fae30df922620ad5cbffe
Romain Menke <11521496+romainmenke@users.noreply.github.com>
2023-12-07 12:33:17 +0100

    release plan : smaller published footprint (#1199)

---
+++ .github/bin/release-plan/npm-prepublish-scripts.mjs
@@ -0,0 +1,35 @@
+import { spawn } from 'child_process';
+import { platform } from 'process';
+
+export async function npmPrepublishScripts(packageDirectory, packageName) {
+	await new Promise((resolve, reject) => {
+		if (process.env.DEBUG) {
+			resolve(true);
+
+			return;
+		}
+
+		const publishCmd = spawn(
+			'npm',
+			[
+				'run',
+				'prepublishOnly',
+				'--if-present'
+			],
+			{
+				stdio: 'inherit',
+				cwd: packageDirectory,
+				shell: platform === 'win32'
+			}
+		);
+
+		publishCmd.on('close', (code) => {
+			if (0 !== code) {
+				reject(new Error(`'npm run prepublishOnly' exited with code ${code} for ${packageName}`));
+				return;
+			}
+
+			resolve(true);
+		});
+	});
+}
