22a2279a602dac4f83e95ea89e2419f0e73556c9
Romain Menke <romainmenke@gmail.com>
2023-12-15 19:36:08 +0100

    wording

--- .github/bin/generate-docs/install.mjs
+++ .github/bin/generate-docs/install.mjs
@@ -12,7 +12,7 @@ installDoc = installDoc.replaceAll('<packageName>', packageJSONInfo.name);
 installDoc = installDoc.replaceAll('<packagePath>', path.join(path.basename(path.dirname(process.cwd())), path.basename(process.cwd())));
 
 if (packageJSONInfo?.csstools?.assumesToProcessBundledCSS) {
-	installDoc = installDoc.replaceAll('<parallelBuildsNotice>', `> [!IMPORTANT]\n> [${packageJSONInfo.csstools.humanReadableName}] assumes to process your complete CSS bundle.<br>If your build tool processes files individually or in parallel the output will be incorrect.<br>Using [\`@csstools/postcss-bundler\`](https://github.com/csstools/postcss-plugins/tree/main/plugin-packs/postcss-bundler) and \`@import\` statements is one way to make sure your CSS is bundled before it is processed by this plugin.\n`);
+	installDoc = installDoc.replaceAll('<parallelBuildsNotice>', `> [!IMPORTANT]\n> [${packageJSONInfo.csstools.humanReadableName}] assumes to process your complete CSS bundle.<br>If your build tool processes files individually or processes files in parallel the output will be incorrect.<br>Using [\`@csstools/postcss-bundler\`](https://github.com/csstools/postcss-plugins/tree/main/plugin-packs/postcss-bundler) and \`@import\` statements is one way to make sure your CSS is bundled before it is processed by this plugin.\n`);
 	installDoc = installDoc.replaceAll('<otherPluginsInWebpack>', `["@csstools/postcss-bundler"]`);
 	// <parallelBuildsNotice>
 } else {
--- plugins/postcss-cascade-layers/INSTALL.md
+++ plugins/postcss-cascade-layers/INSTALL.md
@@ -11,7 +11,7 @@
 - [Grunt](#grunt)
 
 > [!IMPORTANT]
-> [PostCSS Cascade Layers] assumes to process your complete CSS bundle.<br>If your build tool processes files individually or in parallel the output will be incorrect.<br>Using [`@csstools/postcss-bundler`](https://github.com/csstools/postcss-plugins/tree/main/plugin-packs/postcss-bundler) and `@import` statements is one way to make sure your CSS is bundled before it is processed by this plugin.
+> [PostCSS Cascade Layers] assumes to process your complete CSS bundle.<br>If your build tool processes files individually or processes files in parallel the output will be incorrect.<br>Using [`@csstools/postcss-bundler`](https://github.com/csstools/postcss-plugins/tree/main/plugin-packs/postcss-bundler) and `@import` statements is one way to make sure your CSS is bundled before it is processed by this plugin.
 
 
 ## Node
