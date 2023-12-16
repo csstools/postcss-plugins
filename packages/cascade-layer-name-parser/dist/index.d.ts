3d6a00cdf1d47d5cc58e7ee9d2ecfab7ab4d6f25
Romain Menke <romainmenke@gmail.com>
2023-12-16 12:41:28 +0100

    fix

--- packages/cascade-layer-name-parser/dist/index.d.ts
+++ packages/cascade-layer-name-parser/dist/index.d.ts
@@ -26,7 +26,6 @@ export declare function parse(source: string, options?: {
 
 /**
  * Parses an array of {@link https://github.com/csstools/postcss-plugins/tree/main/packages/css-tokenizer/docs/css-tokenizer.csstoken.md | CSSTokens} into a list of cascade layer names.
- *
  */
 export declare function parseFromTokens(tokens: Array<CSSToken>, options?: {
     onParseError?: (error: ParseError) => void;
