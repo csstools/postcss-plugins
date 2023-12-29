0262852c71816801c60ada3ea0081c55f948346f
Romain Menke <romainmenke@gmail.com>
2023-12-29 23:49:48 +0100

    fix

--- plugin-packs/postcss-bundler/test/_sourcemaps.mjs
+++ plugin-packs/postcss-bundler/test/_sourcemaps.mjs
@@ -2,6 +2,7 @@ import postcss from 'postcss';
 import plugin from '@csstools/postcss-bundler';
 import test from 'node:test';
 import assert from 'node:assert';
+import path from 'node:path';
 
 test('sourcemaps', async () => {
 	const result = await postcss([
@@ -19,12 +20,12 @@ test('sourcemaps', async () => {
 		{
 			version: 3,
 			sources: [
-				'test/basic.css',
-				'test/imports/basic.css', /* this has to reflect the path in the inline sourcemap of 'minified-source.css' */
+				'test/basic.css'.split(path.posix.sep).join(path.sep),
+				'test/imports/basic.css'.split(path.posix.sep).join(path.sep), /* this has to reflect the path in the inline sourcemap of 'minified-source.css' */
 			],
 			names: [],
 			mappings: 'AAAA,kCAA0C,ECA1C,OACE,sBAAsB,EAAE,SAC1B,EAEA,OACC,WACD,EAEA,OACC,WACD,EAEA,OACC,kBACD,EAEA,OACC,kBACD,EAEA,OACC,kBACD',
-			file: 'test/basic.css',
+			file: 'test/basic.css'.split(path.posix.sep).join(path.sep),
 			sourcesContent: [
 				'@import url(./imports/minified-source.css);\n',
 				'.foo {\n  --var: /* a comment */; bar: red;\n}\n\n.foo {\n\tcolor: red;\n}\n\n.foo {\n\tcolor:  red;\n}\n\n.foo {\n\tcolor: rgb(0  0 0);\n}\n\n.foo {\n\tcolor: rgb(0 /* a comment */ 0 0);\n}\n\n.foo {\n\tcolor: rgb(0 /* a comment */ /* more comments */ 0 0);\n}\n',
