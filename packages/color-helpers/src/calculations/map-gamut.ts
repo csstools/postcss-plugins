88de37c94915d5899487bb86ac9bb640fa1d1bb2
Romain Menke <romainmenke@gmail.com>
2024-01-04 22:45:22 +0100

    cleanup

--- packages/color-helpers/src/calculations/map-gamut.ts
+++ packages/color-helpers/src/calculations/map-gamut.ts
@@ -1,6 +1,41 @@
-import { binarySearchGamut } from '../calculations/binary-search-gamut';
+import { clip } from '../utils/clip';
+import { OKLCH_to_OKLab } from '../conversions/oklch-to-oklab';
+import { deltaEOK } from './delta-EOK';
 import type { Color } from '../types/color';
+import { inGamut } from '../utils/in-gamut';
 
-export function mapGamut(startOKLCH: Color, toDestination: (x: Color) => Color, fromDestination: (x: Color) => Color): Color {
-	return binarySearchGamut(startOKLCH, toDestination, fromDestination);
+const JND = 0.02;
+const EPSILON = 0.00001;
+
+export function mapGamut(
+	startOKLCH: Color,
+	toDestination: (x: Color) => Color,
+	fromDestination: (x: Color) => Color,
+): Color {
+
+	const current = startOKLCH;
+
+	let min = 0.0;
+	let max = current[1];
+
+	while ((max - min) > EPSILON) {
+		const chroma = (min + max) / 2.0;
+		current[1] = chroma;
+
+		const converted = toDestination(current);
+		if (inGamut(converted)) {
+			min = chroma;
+			continue;
+		}
+
+		const clipped = clip(converted);
+		const delta_e = deltaEOK(OKLCH_to_OKLab(fromDestination(clipped)), OKLCH_to_OKLab(current));
+		if (delta_e < JND) {
+			return clipped;
+		}
+
+		max = chroma;
+	}
+
+	return clip(toDestination([...current]));
 }
