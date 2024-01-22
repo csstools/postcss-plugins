eb79243b1fc97e6898c08afdae9808893b1000cf
Romain Menke <romainmenke@gmail.com>
2024-01-22 22:57:00 +0100

    @csstools/selector-resolve-nested v1.0.2

--- packages/selector-resolve-nested/CHANGELOG.md
+++ packages/selector-resolve-nested/CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changes to Selector Resolve Nested
 
+## 1.0.2
+
+_January 22, 2024_
+
+- Preserve extra/unknown fields on source objects of AST nodes
+
 ### 1.0.1
 
 _January 21, 2024_
--- packages/selector-resolve-nested/test/source-indices.mjs
+++ packages/selector-resolve-nested/test/source-indices.mjs
@@ -4,18 +4,40 @@ import { resolveNestedSelector } from '@csstools/selector-resolve-nested';
 import parser from 'postcss-selector-parser';
 
 test('source indices', async () => {
+	const aNode = Symbol('a');
+	const bNode = Symbol('b');
+
 	const a = parser().astSync('.a .aa');
+	a.source.node = aNode;
+	a.walk((node) => {
+		node.source.node = aNode;
+	});
+
 	const b = parser().astSync('& + .b');
+	b.source.node = bNode;
+	b.walk((node) => {
+		node.source.node = bNode;
+	});
 
 	const ba = resolveNestedSelector(b, a);
 	assert.equal(ba.toString(), ':is(.a .aa) + .b');
 
 	const firstSelector = ba.nodes[0];
+	assert.equal(firstSelector.toString(), ':is(.a .aa) + .b');
+
 	const isPseudo = firstSelector.nodes[0];
 
 	assert.equal(isPseudo.toString(), ':is(.a .aa)');
 	assert.deepEqual(
 		isPseudo.source,
-		{ start: { line: 1, column: 1 }, end: { line: 1, column: 6 } },
+		{ node: aNode, start: { line: 1, column: 1 }, end: { line: 1, column: 6 } },
+	);
+
+	const classNode = firstSelector.nodes[2];
+
+	assert.equal(classNode.toString(), '.b');
+	assert.deepEqual(
+		classNode.source,
+		{ node: bNode, start: { line: 1, column: 5 }, end: { line: 1, column: 6 } },
 	);
 });
