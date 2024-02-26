700ac1b9955f3f37f3bb8f2793b8a3f0f428414e
Romain Menke <11521496+romainmenke@users.noreply.github.com>
2024-02-26 08:17:01 +0100

    improve the regex for gradient function detection (#1302)

--- plugins/postcss-gradients-interpolation-method/src/is-gradient.ts
+++ plugins/postcss-gradients-interpolation-method/src/is-gradient.ts
@@ -1,2 +1,10 @@
-export const GRADIENT_FUNCTION_REGEX = /(repeating-)?(linear|radial|conic)-gradient\(.*?in/i;
+const GRADIENT_FUNCTION_REGEX = /(repeating-)?(linear|radial|conic)-gradient\(/i;
+const IN_KEYWORD_REGEX = /\bin\b/i;
+
+export const IS_GRADIENT_FUNCTION = {
+	test(v: string): boolean {
+		return GRADIENT_FUNCTION_REGEX.test(v) && IN_KEYWORD_REGEX.test(v);
+	},
+};
+
 export const GRADIENT_NAME_REGEX = /^(repeating-)?(linear|radial|conic)-gradient$/i;
