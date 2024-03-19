a5e3eff83fc99d78996385b33289f5541f28e3cd
Romain Menke <romainmenke@gmail.com>
2024-03-19 18:00:12 +0100

    postcss-rewrite-url: once hook

--- plugins/postcss-rewrite-url/CHANGELOG.md
+++ plugins/postcss-rewrite-url/CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changes to PostCSS Rewrite URL
 
+### Unreleased (minor)
+
+- Add support for early transforms in the `Once` hook. This allows you to transform `rewrite-url()` values before any other plugin runs.
+
 ### 1.0.0
 
 _March 17, 2024_
--- plugins/postcss-rewrite-url/src/index.ts
+++ plugins/postcss-rewrite-url/src/index.ts
@@ -1,4 +1,4 @@
-import type { PluginCreator } from 'postcss';
+import type { AtRule, Declaration, PluginCreator, Result } from 'postcss';
 import { TokenType, tokenize } from '@csstools/css-tokenizer';
 import { isCommentNode, isFunctionNode, isTokenNode, isWhitespaceNode, parseCommaSeparatedListOfComponentValues, replaceComponentValues, stringify } from '@csstools/css-parser-algorithms';
 import { serializeString } from './serialize-string';
@@ -32,39 +32,20 @@ const creator: PluginCreator<pluginOptions> = (options?: pluginOptions) => {
 
 	return {
 		postcssPlugin: 'postcss-rewrite-url',
+		Once(root, { result }): void {
+			root.walkDecls((decl) => {
+				rewriteDeclaration(decl, result, rewriter);
+			});
+
+			root.walkAtRules((atRule) => {
+				rewriteAtRule(atRule, result, rewriter);
+			});
+		},
 		Declaration(decl, { result }): void {
-			if (!URL_FUNCTION_CALL_REGEX.test(decl.value)) {
-				return;
-			}
-
-			const context: RewriteContext = {
-				type: 'declaration-value',
-				rootFrom: result.opts.from,
-				from: decl.source?.input.from,
-				property: decl.prop,
-			};
-
-			const modifiedValue = rewrite(rewriter, decl.value, context);
-			if (modifiedValue !== decl.value) {
-				decl.value = modifiedValue;
-			}
+			rewriteDeclaration(decl, result, rewriter);
 		},
 		AtRule(atRule, { result }): void {
-			if (!URL_FUNCTION_CALL_REGEX.test(atRule.params)) {
-				return;
-			}
-
-			const context: RewriteContext = {
-				type: 'at-rule-prelude',
-				rootFrom: result.opts.from,
-				from: atRule.source?.input.from,
-				atRuleName: atRule.name,
-			};
-
-			const modifiedParams = rewrite(rewriter, atRule.params, context);
-			if (modifiedParams !== atRule.params) {
-				atRule.params = modifiedParams;
-			}
+			rewriteAtRule(atRule, result, rewriter);
 		},
 	};
 };
@@ -72,6 +53,42 @@ const creator: PluginCreator<pluginOptions> = (options?: pluginOptions) => {
 creator.postcss = true;
 
 export default creator;
+
+function rewriteDeclaration(decl: Declaration, result: Result, rewriter: Rewriter): void {
+	if(!URL_FUNCTION_CALL_REGEX.test(decl.value)) {
+		return;
+	}
+
+	const context: RewriteContext = {
+		type: 'declaration-value',
+		rootFrom: result.opts.from,
+		from: decl.source?.input.from,
+		property: decl.prop,
+	};
+
+	const modifiedValue = rewrite(rewriter, decl.value, context);
+	if (modifiedValue !== decl.value) {
+		decl.value = modifiedValue;
+	}
+}
+
+function rewriteAtRule(atRule: AtRule, result: Result, rewriter: Rewriter): void {
+	if(!URL_FUNCTION_CALL_REGEX.test(atRule.params)) {
+		return;
+	}
+
+	const context: RewriteContext = {
+		type: 'at-rule-prelude',
+		rootFrom: result.opts.from,
+		from: atRule.source?.input.from,
+		atRuleName: atRule.name,
+	};
+
+	const modifiedParams = rewrite(rewriter, atRule.params, context);
+	if (modifiedParams !== atRule.params) {
+		atRule.params = modifiedParams;
+	}
+}
 
 function rewrite(rewriter: Rewriter, value: string, context: RewriteContext): string {
 	const componentValuesList = parseCommaSeparatedListOfComponentValues(tokenize({ css: value }));
