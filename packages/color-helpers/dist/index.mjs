091744561e75ce3de3054f7de9ce6108d33ebea7
Romain Menke <romainmenke@gmail.com>
2024-04-21 10:58:22 +0200

    update source integrity

--- packages/color-helpers/dist/index.mjs
+++ packages/color-helpers/dist/index.mjs
@@ -18,7 +18,7 @@ function multiplyMatrices(t,_){const n=t.length;let o,e;o=Array.isArray(t[0])?t:
  * @param {number} hue - Hue as degrees 0..360
  * @param {number} sat - Saturation as percentage 0..100
  * @param {number} light - Lightness as percentage 0..100
- * @return {number[]} Array of RGB components 0..1
+ * @return {number[]} Array of sRGB components; in-gamut colors in range [0..1]
  *
  * @license W3C https://www.w3.org/Consortium/Legal/2015/copyright-software-and-document
  * @copyright This software or document includes material copied from or derived from https://github.com/w3c/csswg-drafts/blob/main/css-color-4/hslToRgb.js. Copyright © 2022 W3C® (MIT, ERCIM, Keio, Beihang).
