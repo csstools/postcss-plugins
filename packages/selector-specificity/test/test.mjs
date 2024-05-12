13acb110c7e9f1ca464250352791b8bc7074fc73
Romain Menke <romainmenke@gmail.com>
2024-05-12 09:27:49 +0200

    increase test coverage

--- packages/selector-specificity/test/test.mjs
+++ packages/selector-specificity/test/test.mjs
@@ -155,6 +155,14 @@ assert.deepEqual(calculate(':active-view-transition-type(*)'), { a: 0, b: 1, c: 
 assert.deepEqual(calculate(':active-view-transition-type(*, bar)'), { a: 0, b: 1, c: 0 });
 assert.deepEqual(calculate(':active-view-transition-type(*, *)'), { a: 0, b: 1, c: 0 });
 
+assert.deepEqual(calculate(':lang()'), { a: 0, b: 1, c: 0 });
+assert.deepEqual(calculate(':lang(nl-be)'), { a: 0, b: 1, c: 0 });
+assert.deepEqual(calculate(':lang(nl-be, fr-be)'), { a: 0, b: 1, c: 0 });
+assert.deepEqual(calculate(':lang(nl-be fr-be)'), { a: 0, b: 1, c: 0 });
 assert.deepEqual(calculate(':lang(\\*-Latn)'), { a: 0, b: 1, c: 0 });
 assert.deepEqual(calculate(':lang("*-Latn")'), { a: 0, b: 1, c: 0 });
-assert.deepEqual(calculate(':lang(nl-be, fr-be)'), { a: 0, b: 1, c: 0 });
+
+assert.deepEqual(calculate(':dir()'), { a: 0, b: 1, c: 0 });
+assert.deepEqual(calculate(':dir(rtl)'), { a: 0, b: 1, c: 0 });
+assert.deepEqual(calculate(':dir(rtl, ltr)'), { a: 0, b: 1, c: 0 });
+assert.deepEqual(calculate(':dir(rtl ltr)'), { a: 0, b: 1, c: 0 });
