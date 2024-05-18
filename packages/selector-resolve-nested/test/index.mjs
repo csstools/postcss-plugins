bbd7482ea48cc54f4b0e917f028b8d55ae129e8e
Romain Menke <romainmenke@gmail.com>
2024-05-18 22:41:29 +0200

    increase test coverage

--- packages/selector-resolve-nested/test/index.mjs
+++ packages/selector-resolve-nested/test/index.mjs
@@ -4,11 +4,13 @@ import './compound.mjs';
 import './resolve-nested-selector/lists.mjs';
 import './resolve-nested-selector/multiple-resolves.mjs';
 import './resolve-nested-selector/parent.mjs';
+import './resolve-nested-selector/partial.mjs';
 import './resolve-nested-selector/simple-compound-complex.mjs';
 import './resolve-nested-selector/source-indices.mjs';
 
 import './flatten-nested-selector/lists.mjs';
 import './flatten-nested-selector/multiple-resolves.mjs';
 import './flatten-nested-selector/parent.mjs';
+import './flatten-nested-selector/partial.mjs';
 import './flatten-nested-selector/simple-compound-complex.mjs';
 import './flatten-nested-selector/source-indices.mjs';
