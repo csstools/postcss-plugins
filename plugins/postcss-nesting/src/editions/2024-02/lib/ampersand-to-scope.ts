7beafdc5e65b5876fe7c8751f10803c0c228e3e4
Romain Menke <romainmenke@gmail.com>
2024-05-23 23:33:33 +0200

    fix

--- plugins/postcss-nesting/src/editions/2024-02/lib/ampersand-to-scope.ts
+++ plugins/postcss-nesting/src/editions/2024-02/lib/ampersand-to-scope.ts
@@ -1,42 +1,49 @@
 import type { Container, Node, Result, Rule } from 'postcss';
-import parser from 'postcss-selector-parser';
 import { isAtRule } from '../../shared/lib/is-type-of-rule';
+import mergeSelectors from './merge-selectors';
+import parser from 'postcss-selector-parser';
 
 export default function ampersandToScope(rule: Rule, result: Result): void {
-	let parent: Container<Node> = rule.parent;
+	{
+		let parent: Container<Node> = rule.parent;
 
-	while (parent) {
-		if (parent.type === 'rule') {
-			// Skip any rules that nested.
-			// We only want to process "&" found in unnested rules.
-			return;
+		while (parent) {
+			if (parent.type === 'rule') {
+				// Skip any rules that nested.
+				// We only want to process "&" found in unnested rules.
+				return;
+			}
+
+			if (isAtRule(parent) && parent.name === 'scope') {
+				// Skip @scope rules.
+				// These are newer than nesting and we don't want to break these.
+				return;
+			}
+
+			parent = parent.parent;
 		}
 
-		if (isAtRule(parent) && parent.name === 'scope') {
-			// Skip @scope rules.
-			// These are newer than nesting and we don't want to break these.
+		try {
+			let isNestContaining = false;
+			parser().astSync(rule.selector).walkNesting(() => {
+				isNestContaining = true;
+				return false;
+			});
+
+			if (!isNestContaining) {
+				// Skip any selectors that do not contain an actual `&` token;
+				return;
+			}
+		} catch (err) {
+			rule.warn(result, `Failed to parse selector : "${rule.selector}" with message: "${(err instanceof Error) ? err.message : err}"`);
 			return;
 		}
-
-		parent = parent.parent;
 	}
 
-	let selectorAST: parser.Root;
-	try {
-		selectorAST = parser().astSync(rule.selector);
-	} catch (err) {
-		rule.warn(result, `Failed to parse selector : "${rule.selector}" with message: "${(err instanceof Error) ? err.message : err}"`);
-		return;
-	}
-	if (!selectorAST) {
+	const selectors = mergeSelectors(rule, rule.selector, ':scope', result);
+	if (!selectors) {
 		return;
 	}
 
-	selectorAST.walkNesting((nesting) => {
-		nesting.replaceWith(parser.pseudo({
-			value: ':scope',
-		}));
-	});
-
-	rule.selector = selectorAST.toString();
+	rule.selector = selectors;
 }
