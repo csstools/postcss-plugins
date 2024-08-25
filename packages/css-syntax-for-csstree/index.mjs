dd7129fe56419b7fb8ccc285a02ddf87ef49680f
Romain Menke <romainmenke@gmail.com>
2024-08-25 10:01:25 +0200

    wip

--- packages/css-syntax-for-csstree/index.mjs
+++ packages/css-syntax-for-csstree/index.mjs
@@ -1,72 +1,119 @@
-import fs from 'fs/promises';
-import { fork, definitionSyntax } from 'css-tree';
+import fs from 'node:fs/promises';
+
+import css from '@webref/css';
+import { fork, definitionSyntax, lexer } from 'css-tree';
+
+// TODO: toposort
+// TODO: circular checks
+// TODO: missing types/props checks
 
 
-const propertiesSyntax = {
-	overflow: '| overlay', // csstree/csstree#248
-	width: '| min-intrinsic | -moz-min-content | -moz-available | -webkit-fill-available', // csstree/csstree#242
-	'field-sizing': 'content | fixed',
-	'text-box-edge':
-		'auto | [ text | cap | ex | ideographic | ideographic-ink ] [ text | alphabetic | ideographic | ideographic-ink ]?',
-	'text-box-trim': 'none | trim-start | trim-end | trim-both',
-	'text-spacing-trim': 'normal | space-all | space-first | trim-start',
-	'text-wrap-style': 'auto | balance | pretty | stable',
-	'word-break': '| auto-phrase | auto-phrase',
+const propertiesSyntaxPatches = {
+	// overflow: '| overlay', // csstree/csstree#248
+	// width: '| min-intrinsic | -moz-min-content | -moz-available | -webkit-fill-available', // csstree/csstree#242
+	// 'field-sizing': 'content | fixed',
+	// 'text-box-edge':
+	// 	'auto | [ text | cap | ex | ideographic | ideographic-ink ] [ text | alphabetic | ideographic | ideographic-ink ]?',
+	// 'text-box-trim': 'none | trim-start | trim-end | trim-both',
+	// 'text-spacing-trim': 'normal | space-all | space-first | trim-start',
+	// 'text-wrap-style': 'auto | balance | pretty | stable',
+	// 'word-break': '| auto-phrase | auto-phrase',
+	// 'view-transition-name': 'none | <custom-ident>',
+	// 'anchor-name': 'none | <custom-property-name>#',
 };
 
-const forkedLexer = fork({
-	properties: {},
-	types: {},
-}).lexer;
+function generate_set(source) {
+	const set = Object(null);
 
-{
-	const propertiesSyntax = Object(null);
-
-	for (const key in forkedLexer.properties) {
-		if (!Object.prototype.hasOwnProperty.call(forkedLexer.properties, key)) {
+	for (const key in source) {
+		if (!Object.prototype.hasOwnProperty.call(source, key)) {
 			continue;
 		}
 
-		if (!forkedLexer.properties[key].syntax) {
-			console.log('no syntax for property', key);
+		if (!source[key].syntax) {
+			// console.log('no syntax for', key);
 
 			continue;
 		}
 
-		propertiesSyntax[key] = {
-			'syntax': definitionSyntax.generate(forkedLexer.properties[key].syntax),
+		set[key] = {
+			syntax: definitionSyntax.generate(source[key].syntax),
 		};
 	}
 
+	return set;
+}
+
+function webref_css_to_csstree(definition) {
+	if (definition.newValues) {
+		console.log(definition.values ?? '-');
+		console.log(definition.newValues ?? '-');
+		console.log('------------------');
+	}
+
+	return {
+		syntax: definitionSyntax.parse(definition.value ?? definition.newValues),
+	};
+}
+
+await fs.writeFile(
+	'csstree-properties.json',
+	JSON.stringify(generate_set(lexer.properties), null, '\t'),
+	'utf-8',
+);
+
+await fs.writeFile(
+	'csstree-types.json',
+	JSON.stringify(generate_set(lexer.types), null, '\t'),
+	'utf-8',
+);
+
+{
+	const set = Object(null);
+
+	const parsedFiles = await css.listAll();
+	for (const [, data] of Object.entries(parsedFiles)) {
+		for (const property of data.properties) {
+			if (property.value) {
+				set[property.name] = webref_css_to_csstree(property);
+			} else {
+				console.log(property);
+
+			}
+		}
+	}
+
 	await fs.writeFile(
-		'properties-syntax.json',
-		JSON.stringify(propertiesSyntax, null, '\t'),
+		'webref-css-properties-a.json',
+		JSON.stringify(generate_set(set), null, '\t'),
 		'utf-8',
 	);
 }
 
 {
-	const typesSyntax = Object(null);
+	const set = Object(null);
 
-	for (const key in forkedLexer.types) {
-		if (!Object.prototype.hasOwnProperty.call(forkedLexer.types, key)) {
-			continue;
+	const parsedFiles = await css.listAll();
+	for (const [, data] of Object.entries(parsedFiles)) {
+		console.log(Object.keys(data));
+
+		for (const property of data.properties) {
+			if (property.value) {
+				set[property.name] = property.value ?? property.newValues;
+			} else {
+				console.log(property);
+			}
 		}
-
-		if (!forkedLexer.types[key].syntax) {
-			console.log('no syntax for type', key);
-
-			continue;
-		}
-
-		typesSyntax[key] = {
-			'syntax': definitionSyntax.generate(forkedLexer.types[key].syntax),
-		};
 	}
 
+	const forkedLexer = fork({
+		properties: set,
+		types: {},
+	}).lexer;
+
 	await fs.writeFile(
-		'types-syntax.json',
-		JSON.stringify(typesSyntax, null, '\t'),
+		'webref-css-properties-b.json',
+		JSON.stringify(generate_set(forkedLexer.properties), null, '\t'),
 		'utf-8',
 	);
 }
