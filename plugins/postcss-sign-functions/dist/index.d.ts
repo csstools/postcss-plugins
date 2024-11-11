a864d24a76939fa610d57b331ddf89925b9e88aa
Romain Menke <romainmenke@gmail.com>
2024-11-11 10:21:15 +0100

    rebuild

--- plugins/postcss-sign-functions/dist/index.d.ts
+++ plugins/postcss-sign-functions/dist/index.d.ts
@@ -3,7 +3,7 @@ import type { PluginCreator } from 'postcss';
 declare const creator: PluginCreator<pluginOptions>;
 export default creator;
 
-/** postcss-stepped-value-functions plugin options */
+/** postcss-sign-functions plugin options */
 export declare type pluginOptions = {
     /** Preserve the original notation. default: true */
     preserve?: boolean;
