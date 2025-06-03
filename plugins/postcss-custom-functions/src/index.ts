2ddd14a521faa09b1bba993210e9ab5512b44756
Romain Menke <romainmenke@gmail.com>
2025-06-03 08:20:43 +0200

    wip

--- plugins/postcss-custom-functions/src/index.ts
+++ plugins/postcss-custom-functions/src/index.ts
@@ -31,6 +31,165 @@ const creator: PluginCreator<pluginOptions> = (opts?: pluginOptions) => {
 			let customFunctions: Map<string, CustomFunctionAndNode> = new Map();
 			const callingContexts: Array<CallingContext> = [];
 
+			class CallingContext {
+				static counter = 0;
+
+				id: string;
+
+				rootElement: ContainerWithChildren;
+				element: ContainerWithChildren;
+				property: Declaration;
+				parent: CallingContext | null = null;
+
+				function: FunctionNode;
+				customFunction: CustomFunction;
+				customFunctionBody: AtRule;
+
+				registrations: Array<AtRule> = [];
+
+				argumentRule: ContainerWithChildren;
+				bodyRule: ContainerWithChildren;
+
+				resultIdent: string;
+
+				varCounter: number = 0;
+
+				invalid: boolean = false;
+
+				// TODO: some ident mapping
+
+				constructor(
+					rootElement: ContainerWithChildren,
+					element: ContainerWithChildren,
+					property: Declaration,
+
+					fn: FunctionNode,
+					customFunction: CustomFunction,
+					customFunctionBody: AtRule,
+
+					atRule: (defaults?: AtRuleProps) => AtRule,
+
+					parent: CallingContext | null = null,
+				) {
+					this.id = (CallingContext.counter++).toString(36);
+
+					this.rootElement = rootElement;
+					this.element = element;
+					this.property = property;
+
+					this.function = fn;
+					this.customFunction = customFunction;
+					this.customFunctionBody = customFunctionBody;
+
+					this.parent = parent;
+
+					{
+						const varCounterValue = (++this.varCounter).toString(36);
+						this.resultIdent = generateDashedIdent('custom-function-call', varCounterValue, this.id);
+
+						const args = parseCommaSeparatedListOfComponentValues(this.function.value.flatMap(x => x.tokens()));
+						if (args.length > this.customFunction.parameters.length) {
+							this.invalid = true;
+						}
+
+						this.customFunction.parameters.forEach((parameter) => {
+							const parameterName = parameter.getName();
+							const parameterType = parameter.getArgumentType() || '*';
+
+							const argumentCustomProperty = atRule({
+								name: 'property',
+								params: generateDashedIdent('function-argument', parameterName, this.id),
+								nodes: [
+									this.property.clone({
+										prop: 'syntax',
+										value: `"${parameterType}"`,
+									}),
+									this.property.clone({
+										prop: 'inherit',
+										value: "false",
+									})
+								],
+								source: this.property.source
+							});
+
+							this.registrations.push(argumentCustomProperty);
+
+							const bodyCustomProperty = atRule({
+								name: 'property',
+								params: generateDashedIdent('function-body', parameterName, this.id),
+								nodes: [
+									this.property.clone({
+										prop: 'syntax',
+										value: '"*"',
+									}),
+									this.property.clone({
+										prop: 'inherit',
+										value: "false",
+									})
+								],
+								source: this.property.source
+							});
+
+							this.registrations.push(bodyCustomProperty);
+						});
+
+						const returnType = this.customFunction.getReturnType();
+						if (returnType) {
+							const customProperty = atRule({
+								name: 'property',
+								params: generateDashedIdent('return-value', '', this.id),
+								nodes: [
+									this.property.clone({
+										prop: 'syntax',
+										value: `"${returnType}"`,
+									}),
+									this.property.clone({
+										prop: 'inherit',
+										value: "false",
+									})
+								],
+								source: this.property.source
+							});
+
+							this.registrations.push(customProperty);
+						}
+
+						this.argumentRule = this.element.clone({ nodes: [] });
+
+						this.customFunction.parameters.forEach((parameter, index) => {
+							const parameterName = parameter.getName();
+							const defaultValue = parameter.getDefaultValue();
+
+							if (defaultValue) {
+								this.argumentRule.append(this.property.clone({
+									prop: generateDashedIdent('function-argument', parameterName, this.id),
+									value: parameter.getDefaultValue(),
+								}));
+							}
+
+							const argumentValue = args[index];
+							// TODO: `{1, 2}` -> `1, 2`
+							if (argumentValue) {
+								this.argumentRule.append(this.property.clone({
+									prop: generateDashedIdent('function-argument', parameterName, this.id),
+									value: stringify([argumentValue]),
+								}));
+							}
+						});
+
+						this.bodyRule = this.element.clone({
+							nodes: [
+								...(this.customFunctionBody.nodes?.map((x) => x.clone()) ?? [])
+							]
+						});
+
+						this.bodyRule.walkDecls(/^result$/i, (resultDecl) => {
+							resultDecl.prop = this.resultIdent;
+						});
+					}
+				}
+			}
+
 			return {
 				postcssPlugin: 'postcss-custom-functions',
 				Once(root, { result }): void {
@@ -140,162 +299,3 @@ function generateDashedIdent(type: string, name: string, id: string): string {
 creator.postcss = true;
 
 export default creator;
-
-class CallingContext {
-	static counter = 0;
-
-	id: string;
-
-	rootElement: ContainerWithChildren;
-	element: ContainerWithChildren;
-	property: Declaration;
-	parent: CallingContext | null = null;
-
-	function: FunctionNode;
-	customFunction: CustomFunction;
-	customFunctionBody: AtRule;
-
-	registrations: Array<AtRule> = [];
-
-	argumentRule: ContainerWithChildren;
-	bodyRule: ContainerWithChildren;
-
-	resultIdent: string;
-
-	varCounter: number = 0;
-
-	invalid: boolean = false;
-
-	// TODO: some ident mapping
-
-	constructor(
-		rootElement: ContainerWithChildren,
-		element: ContainerWithChildren,
-		property: Declaration,
-
-		fn: FunctionNode,
-		customFunction: CustomFunction,
-		customFunctionBody: AtRule,
-
-		atRule: (defaults?: AtRuleProps) => AtRule,
-
-		parent: CallingContext | null = null,
-	) {
-		this.id = (CallingContext.counter++).toString(36);
-
-		this.rootElement = rootElement;
-		this.element = element;
-		this.property = property;
-
-		this.function = fn;
-		this.customFunction = customFunction;
-		this.customFunctionBody = customFunctionBody;
-
-		this.parent = parent;
-
-		{
-			const varCounterValue = (++this.varCounter).toString(36);
-			this.resultIdent = generateDashedIdent('custom-function-call', varCounterValue, this.id);
-
-			const args = parseCommaSeparatedListOfComponentValues(this.function.value.flatMap(x => x.tokens()));
-			if (args.length > this.customFunction.parameters.length) {
-				this.invalid = true;
-			}
-
-			this.customFunction.parameters.forEach((parameter) => {
-				const parameterName = parameter.getName();
-				const parameterType = parameter.getArgumentType() || '*';
-
-				const argumentCustomProperty = atRule({
-					name: 'property',
-					params: generateDashedIdent('function-argument', parameterName, this.id),
-					nodes: [
-						this.property.clone({
-							prop: 'syntax',
-							value: `"${parameterType}"`,
-						}),
-						this.property.clone({
-							prop: 'inherit',
-							value: "false",
-						})
-					],
-					source: this.property.source
-				});
-
-				this.registrations.push(argumentCustomProperty);
-
-				const bodyCustomProperty = atRule({
-					name: 'property',
-					params: generateDashedIdent('function-body', parameterName, this.id),
-					nodes: [
-						this.property.clone({
-							prop: 'syntax',
-							value: '"*"',
-						}),
-						this.property.clone({
-							prop: 'inherit',
-							value: "false",
-						})
-					],
-					source: this.property.source
-				});
-
-				this.registrations.push(bodyCustomProperty);
-			});
-
-			const returnType = this.customFunction.getReturnType();
-			if (returnType) {
-				const customProperty = atRule({
-					name: 'property',
-					params: generateDashedIdent('return-value', '', this.id),
-					nodes: [
-						this.property.clone({
-							prop: 'syntax',
-							value: `"${returnType}"`,
-						}),
-						this.property.clone({
-							prop: 'inherit',
-							value: "false",
-						})
-					],
-					source: this.property.source
-				});
-
-				this.registrations.push(customProperty);
-			}
-
-			this.argumentRule = this.element.clone({ nodes: [] });
-
-			this.customFunction.parameters.forEach((parameter, index) => {
-				const parameterName = parameter.getName();
-				const defaultValue = parameter.getDefaultValue();
-
-				if (defaultValue) {
-					this.argumentRule.append(this.property.clone({
-						prop: generateDashedIdent('function-argument', parameterName, this.id),
-						value: parameter.getDefaultValue(),
-					}));
-				}
-
-				const argumentValue = args[index];
-				// TODO: `{1, 2}` -> `1, 2`
-				if (argumentValue) {
-					this.argumentRule.append(this.property.clone({
-						prop: generateDashedIdent('function-argument', parameterName, this.id),
-						value: stringify([argumentValue]),
-					}));
-				}
-			});
-
-			this.bodyRule = this.element.clone({
-				nodes: [
-					...(this.customFunctionBody.nodes?.map((x) => x.clone()) ?? [])
-				]
-			});
-
-			this.bodyRule.walkDecls(/^result$/i, (resultDecl) => {
-				resultDecl.prop = this.resultIdent;
-			});
-		}
-	}
-}
