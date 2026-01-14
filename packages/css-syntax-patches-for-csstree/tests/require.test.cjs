dec8fc596d5a291f0e84781775b76983868a5408
Romain Menke <romainmenke@gmail.com>
2026-01-14 07:48:43 +0100

    fix

--- packages/css-syntax-patches-for-csstree/tests/require.test.cjs
+++ packages/css-syntax-patches-for-csstree/tests/require.test.cjs
@@ -1,4 +1,8 @@
 const assert = require('node:assert');
-const syntax_patches = require('@csstools/css-syntax-patches-for-csstree');
+const syntax_patches_a = require('@csstools/css-syntax-patches-for-csstree');
 
-assert.ok(syntax_patches.next);
+assert.ok(syntax_patches_a.next);
+
+const syntax_patches_b = require('@csstools/css-syntax-patches-for-csstree/dist/index.json');
+
+assert.ok(syntax_patches_b.next);
