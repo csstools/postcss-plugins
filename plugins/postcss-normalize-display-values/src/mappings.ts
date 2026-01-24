0eb6ab0c56503756c243970db0740f08f18dde63
Romain Menke <romainmenke@gmail.com>
2026-01-24 18:59:10 +0100

    fix plugins/postcss-normalize-display-values

--- plugins/postcss-normalize-display-values/src/mappings.ts
+++ plugins/postcss-normalize-display-values/src/mappings.ts
@@ -2,23 +2,61 @@
  * Specification: https://www.w3.org/TR/css-display-3/#the-display-properties
  */
 export default new Map([
-	['block,flex', 'flex'],
+	['flow', 'block'],
+
 	['block,flow', 'block'],
+	['flow,block', 'block'],
+
+	['flow-root,block', 'flow-root'],
 	['block,flow-root', 'flow-root'],
-	['block,grid', 'grid'],
-	['block,table', 'table'],
-	['inline,block', 'inline-block'],
-	['inline,flex', 'inline-flex'],
+
 	['inline,flow', 'inline'],
-	['inline,flow,list-item', 'inline list-item'],
+	['flow,inline', 'inline'],
+
 	['inline,flow-root', 'inline-block'],
-	['inline,grid', 'inline-grid'],
-	['inline,ruby', 'ruby'],
-	['inline,table', 'inline-table'],
-	['list-item,block,flow', 'list-item'],
-	['ruby-base,flow', 'ruby-base'],
-	['ruby-text,flow', 'ruby-text'],
+	['flow-root,inline', 'inline-block'],
+
 	['run-in,flow', 'run-in'],
-	['table-caption,flow', 'table-caption'],
-	['table-cell,flow', 'table-cell'],
+	['flow,run-in', 'run-in'],
+
+	['block,list-item', 'list-item'],
+	['list-item,block', 'list-item'],
+
+	['flow,list-item', 'list-item'],
+	['list-item,flow', 'list-item'],
+
+	['block,flow,list-item', 'list-item'],
+	['block,list-item,flow', 'list-item'],
+	['list-item,block,flow', 'list-item'],
+	['list-item,flow,block', 'list-item'],
+	['flow,block,list-item', 'list-item'],
+	['flow,list-item,block', 'list-item'],
+
+	['inline,flow,list-item', 'inline list-item'],
+	['inline,list-item,flow', 'inline list-item'],
+	['list-item,inline,flow', 'inline list-item'],
+	['list-item,flow,inline', 'inline list-item'],
+	['flow,inline,list-item', 'inline list-item'],
+	['flow,list-item,inline', 'inline list-item'],
+
+	['block,flex', 'flex'],
+	['flex,block', 'flex'],
+
+	['inline,flex', 'inline-flex'],
+	['flex,inline', 'inline-flex'],
+
+	['block,grid', 'grid'],
+	['grid,block', 'grid'],
+
+	['inline,grid', 'inline-grid'],
+	['grid,inline', 'inline-grid'],
+
+	['inline,ruby', 'ruby'],
+	['ruby,inline', 'ruby'],
+
+	['block,table', 'table'],
+	['table,block', 'table'],
+
+	['inline,table', 'inline-table'],
+	['table,inline', 'inline-table'],
 ]);
