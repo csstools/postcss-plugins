e557344fd4501343f40d7aae7b5f113c3f18e16f
Romain Menke <romainmenke@gmail.com>
2026-08-30 18:09:21 +0200

    postcss-private-rule

---
+++ plugins/postcss-private-rule/src/valid-atrules.ts
@@ -0,0 +1,44 @@
+import type { AtRule, Rule } from "postcss";
+
+const IS_NESTING_GROUP_RULE_REGEX = /^(container|layer|media|scope|starting-style|supports)$/i;
+
+export const IS_PRIVATE_RULE_REGEX = /^private$/i;
+
+const IS_KEYFRAMES_RULE_REGEX = /^keyframes$/i;
+
+export function findStyleRule(atRule: AtRule): false|Rule {
+	if (!atRule.parent) {
+		return false;
+	}
+
+	if (atRule.parent.type === 'rule') {
+		if (atRule.parent.parent?.type === 'atrule' && IS_KEYFRAMES_RULE_REGEX.test(atRule.parent.parent?.name)) {
+			return false;
+		}
+
+		return atRule.parent;
+	}
+
+	if (atRule.parent.type === 'atrule' && IS_NESTING_GROUP_RULE_REGEX.test(atRule.parent.name)) {
+		return findStyleRule(atRule.parent);
+	}
+
+	return false;
+}
+
+
+export function findPrivateRule(atRule: AtRule): false | AtRule {
+	if (atRule.parent?.type !== 'atrule') {
+		return false;
+	}
+
+	if (IS_NESTING_GROUP_RULE_REGEX.test(atRule.parent.name)) {
+		return findPrivateRule(atRule.parent);
+	}
+
+	if (IS_PRIVATE_RULE_REGEX.test(atRule.parent.name)) {
+		return atRule.parent;
+	}
+
+	return false;
+}
